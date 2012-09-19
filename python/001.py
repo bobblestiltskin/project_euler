30df63a0582dda1592a9818bd16b5881aca17145
Bob Wilkinson <bob@fourtheye.org>
2012-09-19 15:04:47 +0100

    parameterise max

---
+++ python/001.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import argparse
+
+def main():
+  """ this computes projecteuler.net problem 001 """
+  parser = argparse.ArgumentParser()
+  parser.add_argument("limit", type=int, help="Upper limit to which we compute factors of 3 and 5")
+
+  args = parser.parse_args()
+  print 'UPPER LIMIT is', args.limit
+
+  total = 0
+  
+  for i in range(0, args.limit):
+    if (((i % 3) == 0) or ((i % 5) == 0)):
+      total += i 
+
+  print 'total is', total
+  return 0
+
+if __name__ == "__main__":
+    main()
