5b777b947ce5aa075c19feb3b2b342a41a7c8114
Bob Wilkinson <bob@fourtheye.org>
2012-09-24 17:34:09 +0100

    re-order

--- perl/009.pl
+++ perl/009.pl
@@ -5,9 +5,11 @@ my $stop = 1000;
 for (my $a = 1; $a < $stop; $a++) {
   for (my $b = $a + 1; $b < $stop; $b++) {
     for (my $c = $b + 1; $c < $stop; $c++) {
-      if (($a * $a) + ($b * $b) == ($c * $c)) {
-        print "($a, $b, $c) -> ",$a*$b*$c,"\n" if (($a + $b +$c) == $stop);
+      if ((($a + $b +$c) == $stop) and (($a * $a) + ($b * $b) == ($c * $c))) {
+        print "($a, $b, $c) -> ",$a*$b*$c,"\n";
+        exit(0);
       }
     }
   }
 }
+exit(1);
