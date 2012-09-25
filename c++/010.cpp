77803fac4f756364822f5d7863a218e5579c3e3e
Bob Wilkinson <bob@fourtheye.org>
2012-09-25 16:58:16 +0100

    new files

---
+++ c++/010.cpp
@@ -0,0 +1,53 @@
+// Copyright 2012 Bob Wilkinson <bob@fourtheye.org>
+
+// This is a solution for http://projecteuler.net/problem=10
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <sstream>
+#include <vector>
+
+int is_prime(const int, const std::vector<int> &);
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Need 1 arg - the ordinal of the prime to find\n");
+    exit(1);
+  }
+  int last;
+  std::stringstream ssout(argv[1]);
+  ssout >> last;
+
+  std::vector<int> primes;
+  primes.push_back(2);
+  primes.push_back(3);
+
+  int test = primes[primes.size() - 1] + 2;
+  while (test < last) {
+    if (is_prime(test, primes)) {
+      primes.push_back(test);
+    }
+    test += 2;
+  }
+  int64_t sum = 0;
+  for (int i = 0; i < primes.size(); ++i) {
+    sum += primes[i];
+  }
+  printf("SUM of all primes less than %d is %ld\n", last, sum);
+  exit(0);
+}
+
+int is_prime(const int test, const std::vector<int> & primes) {
+  int root = static_cast<int>(sqrt(test));
+
+  for (std::vector<int>::const_iterator it = primes.begin();
+       it < primes.end();
+       it++) {
+    if (*it > root)
+      return 1;
+    if (!(test % *it))
+      return 0;
+  }
+  return 0;
+}
---
+++ c/010.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    printf("Need 1 arg - the ordinal of the prime to find\n");
+    exit(1);
+  }
+  int last = atoi(argv[1]);
+
+  int *primes = (int *) calloc(2, sizeof(int));
+  *primes = 2;
+  *(primes+1) = 3;
+  int numprimes = 2;
+
+  int test = primes[numprimes - 1] + 2;
+  while (test < last)
+  {
+    if (is_prime(test, numprimes, primes))
+    {
+      primes = realloc(primes, sizeof(int)*(numprimes+1));
+      primes[numprimes++] = test;
+    }
+    test += 2;
+  }
+  int64_t sum = 0;
+  int i;
+  for (i=0; i < numprimes; ++i)
+  {
+    sum += primes[i];
+  }
+  printf("SUM of all primes less than %d is %ld\n", last, sum);    
+  free(primes);
+  exit(0);
+}
+
+int is_prime(int test, int numprimes, int *primes)
+{
+  int root = (int) sqrt(test);
+  int i;
+  for (i=0; i<numprimes; i++)
+  {
+    if (primes[i] > root)
+      return 1;
+    if (!(test % primes[i]))
+      return 0;
+  }
+  return 0;
+}
