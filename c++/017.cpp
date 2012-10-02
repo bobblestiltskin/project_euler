7e5b59e705bd8c54f7baee9879b14387166fee7f
Bob Wilkinson <bob@fourtheye.org>
2012-10-02 17:37:31 +0100

    drop unused variables

--- c++/017.cpp
+++ c++/017.cpp
@@ -16,13 +16,9 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 */
 
-  const int UNITS = 9;
-  const int TENS = 90;
-
   const std::string onek = "one thousand";
   const int hcount = 7; /* "hundred" */
   const int handcount = 10; /* "hundred and " */
-  int count = 0;
   const std::vector< std::string > units({"one", "two", "three", "four",
   "five", "six", "seven", "eight", "nine"});
   const std::vector< std::string > tens({"ten", "eleven", "twelve", "thirteen",
@@ -43,6 +39,8 @@ NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-
   "eightyfive", "eightysix", "eightyseven", "eightyeight", "eightynine",
   "ninety", "ninetyone", "ninetytwo", "ninetythree", "ninetyfour",
   "ninetyfive", "ninetysix", "ninetyseven", "ninetyeight", "ninetynine"});
+
+  int count = 0;
 
   for (std::vector< std::string >::const_iterator it = units.begin();
        it < units.end(); it++)
