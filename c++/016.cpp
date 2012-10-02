de577c7e8cc501479c576e18be6dd0d4a65a57a8
Bob Wilkinson <bob@fourtheye.org>
2012-10-02 17:21:55 +0100

    after running cpplint.py

--- c++/016.cpp
+++ c++/016.cpp
@@ -1,3 +1,7 @@
+// Copyright 2012 Bob Wilkinson <bob@fourtheye.org>
+
+// This is a solution for http://projecteuler.net/problem=16
+
 #include <cstdio>
 #include <sstream>
 #include <string>
@@ -6,8 +10,7 @@ const int ASCII = 48;
 
 std::string double_string(std::string);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Need 1 arg\n");
     return(1);
@@ -20,22 +23,22 @@ int main(int argc, char **argv)
 // we could store the raw numbers for a little better performance
   std::string in_string = "1";
 
-  for (int i=0; i < max; ++i) {
+  for (int i = 0; i < max; ++i) {
     in_string = double_string(in_string);
   }
   printf("STRING is %s\n", in_string.c_str());
   int sum = 0;
-  std::string::iterator it;
-  for (std::string::iterator it = in_string.begin(); it < in_string.end(); it++) {
+  for (std::string::iterator it = in_string.begin();
+       it < in_string.end();
+       it++)
     sum += *it - ASCII;
-  }
+
   printf("SUM of digits in string is %d\n", sum);
 
   return(0);
 }
 
-std::string double_string(std::string in)
-{
+std::string double_string(std::string in) {
 /*
   given a string containing an integer this function will double the value
   the string will be extended if necessary to hold the number
@@ -43,8 +46,8 @@ std::string double_string(std::string in)
 
   int first = in.at(0) - ASCII; /* use ASCII in string */
   int ra = 0;
-  if ((first == 5) || (first == 6) || (first == 7) || (first == 8) || (first == 9))
-  {
+  if ((first == 5) || (first == 6) || (first == 7) ||
+      (first == 8) || (first == 9)) {
 /*  extend string by a character */
     in.resize(in.size() + 1);
     ra = 1;
@@ -52,12 +55,13 @@ std::string double_string(std::string in)
 
 /* walk backwards through the string doubling as we go */
   int carry = 0;
-//  for (std::string::reverse_iterator rit = in.rbegin() ; rit < in.rend(); rit++ )
-  for (int j = in.size(); j > 0; --j)
-  {
+//  for (std::string::reverse_iterator rit = in.rbegin();
+//         rit < in.rend(); rit++ )
+//  mmm maybe not this walks backwards and we want to modify different points
+//  so doing it explicitly using indexing is easier
+  for (int j = in.size(); j > 0; --j) {
     int i = j - 1;
-    switch(in.at(i)-ASCII)
-    {
+    switch (in.at(i)-ASCII) {
       case 0:
         in.at(i+ra) = 0 + ASCII + carry;
         carry = 0;
@@ -68,7 +72,7 @@ std::string double_string(std::string in)
         break;
       case 2:
         in.at(i+ra) = 4 + ASCII + carry;
-	carry = 0;
+        carry = 0;
         break;
       case 3:
         in.at(i+ra) = 6 + ASCII + carry;
@@ -100,8 +104,7 @@ std::string double_string(std::string in)
         break;
     }
   }
-  if (carry)
-  {
+  if (carry) {
     in.at(0) = 0 + ASCII + carry;
     carry = 0;
   }
