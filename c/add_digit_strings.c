ba089926374a3913d164427b57d5884b403603f1
Bob Wilkinson <bob@fourtheye.org>
2012-10-08 17:11:12 +0100

    better tests

--- c/add_digit_strings.c
+++ c/add_digit_strings.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define BASE 10
 
@@ -8,36 +9,47 @@ int add_digit_strings(char *in_1_string, char *in_2_string, char *out_string)
 {
   /* generate out_string by adding in_2_string to in_1_string */
 
+#ifdef DEBUG
 printf("ENTERING add_digit_strings IN 1 STRING is %s IN 2 STRING is %s\n", in_1_string, in_2_string);
-//printf("ENTERING add_digit_strings\n");
-//printf("ILEN1 is %d and IN 1 STRING is %s\n", (int) strlen(in_2_string), in_2_string);
-//printf("ILEN2 is %d and IN 2 STRING is %s\n", (int) strlen(in_1_string), in_1_string);
+#endif
 
-  int maxlen, minlen;
+  /* if the second string is shorter than the first, swap them */
   if (strlen(in_2_string) < strlen(in_1_string)) 
   {
     char *tmp = in_2_string;
     in_2_string = in_1_string;
     in_1_string = tmp;
   }
-  minlen = strlen(in_1_string);
-  maxlen = strlen(in_2_string);
+  int minlen = strlen(in_1_string);
+  int maxlen = strlen(in_2_string);
 
   int carry = 0;
   int j;
+  /* walk backwards along the shorter string and add elements */
   for (j = minlen; j > 0; --j)
   {
-    int index = ((int) strlen(in_2_string))-minlen+j-1;
-    int tmp = *(in_1_string+j-1) - '0' + *(in_2_string+index) - '0' + carry;
+    int index = maxlen - minlen + j - 1;
+    int in1 = *(in_1_string+j-1) - '0';
+    assert(in1 >= 0);
+    assert(in1 <= 9);
+    int in2 = *(in_2_string+index) - '0';
+    assert(in2 >= 0);
+    assert(in2 <= 9);
+    int tmp = in1 + in2 + carry;
     *(out_string+index) = tmp % BASE + '0';
     carry = tmp / BASE;
   }
+  /* then handle any character positions only in longer string */
   for (j = maxlen - minlen; j > 0; --j)
   {
-    int tmp = *(in_2_string+j-1) - '0' + carry;
+    int in2 = *(in_2_string+j-1) - '0';
+    assert(in2 >= 0);
+    assert(in2 <= 9);
+    int tmp = in2 + carry;
     *(out_string+j-1) = tmp % BASE + '0';
     carry = tmp / BASE;
   }
+  /* need to move the string characters */
   if (carry)
   {
     out_string = realloc(out_string, strlen(out_string) + 2);
@@ -45,10 +57,10 @@ printf("ENTERING add_digit_strings IN 1 STRING is %s IN 2 STRING is %s\n", in_1_
       *(out_string+j) = *(out_string+j-1);
     *out_string = carry + '0';
   }
-//printf("END ILEN is %d and IN1 STRING is %s\n", (int) strlen(in_2_string), in_2_string);
-//printf("END ILEN is %d and IN1 STRING is %s\n", (int) strlen(in_1_string), in_1_string);
-//printf("END OLEN is %d and OUT STRING is %s\n", (int) strlen(out_string), out_string);
+
+#ifdef DEBUG
 printf("LEAVING add_digit_strings IN 1 STRING is %s IN 2 STRING is %s and OUT STRING is %s\n", in_1_string, in_2_string, out_string);
+#endif
 
   return 0;
 }
--- c/test_add_digit_strings.c
+++ c/test_add_digit_strings.c
@@ -7,32 +7,51 @@
 /* and we should use strnlen rather than strlen etc. yada */
 
 int add_digit_strings(char *, char *, char *);
+int call_add_digit_strings(const int, const char **, const char **, const char **);
 
 int main(int argc, char **argv)
 {
-  if (argc != 3)
-  {
-    printf("Need 2 arg\n");
-    return(1);
-  }
+  const char *input1[2] = {
+    "75415123591065981965009485944457939790477550112631613307953910218357656560185790391063809273661832958588846804708867683435301180538880000000000000000000000",
+    "848420140399492297106356716875151822642872438767105649714481489956523636302090141899467854328695620784124526552974761438647138281062400000000000000000000000"
+  };
+  const char *input2[2] = {
+    "848420140399492297106356716875151822642872438767105649714481489956523636302090141899467854328695620784124526552974761438647138281",
+    "7541512359106598196500948594445793979047755011263161330795391021835765656018579039106380927366183295858884680470886768343530118053888",
+  };
+  const char *output[2] = {
+    "75415123591065981965009486792878080189969847218988330183105732861230095327291440105545299230185469260678988704176722012130921964663406552974761438647138281",
+    "848420140399492297106364258387510929241068939715700095508460537711534899463420937290489690094351639363163632933902127621942997165742870886768343530118053888"
+  };
 
-  int lena = strlen(argv[1]);
-  char *a = (char *) calloc(lena + 1, sizeof(char));
-  strcpy(a, argv[1]);
-  int lenb = strlen(argv[2]);
-  char *b = (char *) calloc(lenb + 1, sizeof(char));
-  strcpy(b, argv[2]);
+//printf("BEFORE a is %s and B is %s and C is %s\n", a, b, c);
+//  int status = add_digit_strings(a, b, c);
+//printf("AFTER a is %s and B is %s and C is %s\n", a, b, c);
+
+  int status = call_add_digit_strings(0, input1, input2, output);
+  if (!status)
+    status = call_add_digit_strings(1, input1, input2, output);
+
+  return(status);
+}
+
+int call_add_digit_strings(const int index, const char **input1, const char **input2, const char **output)
+{
+  char *a = (char *) calloc(strlen(input1[index]) + 1, sizeof(char));
+  strncpy(a, input1[index], strlen(input1[index]));
+  int lena = strlen(a);
+
+  char *b = (char *) calloc(strlen(input2[index]) + 1, sizeof(char));
+  strncpy(b, input2[index], strlen(input2[index]));
+  int lenb = strlen(b);
+
   int maxlen = (lena >lenb) ? lena : lenb;
   char *c = (char *) calloc(maxlen + 1, sizeof(char));
 
-//  strcpy(a, "75415123591065981965009485944457939790477550112631613307953910218357656560185790391063809273661832958588846804708867683435301180538880000000000000000000000");
-//  strcpy(a, "99999");
-//  char *b = (char *) calloc(158, sizeof(char));
-//  strcpy(b, "848420140399492297106356716875151822642872438767105649714481489956523636302090141899467854328695620784124526552974761438647138281062400000000000000000000000");
-//  strcpy(b, "999999");
-
-printf("BEFORE a is %s and B is %s and C is %s\n", a, b, c);
   int status = add_digit_strings(a, b, c);
-printf("AFTER a is %s and B is %s and C is %s\n", a, b, c);
-  return(status);
+  if (strncmp(c, output[index], strlen(output[index])))
+    printf("%s + %s is %s and should be %s\n", a, b, c, output[index]);
+
+  return status;
 }
+
