deed56f0394559898483bb05f9282e68d039681a
Bob Wilkinson <bob@fourtheye.org>
2012-10-08 17:47:54 +0100

    itidied

--- c/mul_int_string.c
+++ c/mul_int_string.c
@@ -1,53 +1,39 @@
 #include <stdio.h>
-//#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define BASE 10
 
-/* this really needs assetions to guarantee that we are working with numbers in the strings. */
-/* and we should use strnlen rather than strlen etc. yada */
+int mul_digit_string(const int, const char *, char *);
+int add_digit_strings(char *, char *, char *);
 
 int mul_int_string(int alpha, char *in_string, char *out_string)
 {
+#ifdef DEBUG
 printf("ENTERING mul_int_string with alpha of %d and in_string of %s and outstring of %s\n", alpha, in_string, out_string);
+#endif
+
   /* multiplies the input string in_string by the integer alpha */
+
   strcpy(out_string, "0");
-
-
- int in_length = strlen(in_string); 
-//printf("1. LEN in_string is %d\n", in_length);
-//printf("1. LEN in_string is %d\n", (int) strlen(in_string));
-//printf("1. LEN out_string is %d\n", (int) strlen(out_string));
+  int in_length = strlen(in_string); 
 
   do {
-//    out_string = realloc(out_string, strlen(out_string) + 1);
-//printf("2. LEN out_string is %d\n", (int) strlen(out_string));
     int modulo = alpha % BASE;
-    printf("ALPHA is %d and MODULO is %d\n", alpha, modulo);
+//    printf("ALPHA is %d and MODULO is %d\n", alpha, modulo);
     char *row = (char *) calloc(in_length + 1, sizeof(char));
-//printf("OK1\n");
-//printf("in_string is %s\n", in_string);
     int mds_ret = mul_digit_string(modulo, in_string, row);
-//printf("OK1\n");
-//printf("row is %s\n", row);
-//printf("3. LEN out_string is %d\n", (int) strlen(out_string));
+    assert (mds_ret == 0);
     add_digit_strings(row, out_string, out_string);
-//printf("4. LEN out_string is %d\n", (int) strlen(out_string));
-printf("BACK FROM add_digit_strings with outstring of %s\n", out_string);
-//printf("ROW is %d and ROW STRING is %s\n", (int) strlen(row), row);
-//printf("OLEN is %d and OUT STRING is %s\n", (int) strlen(out_string), out_string);
-//printf("out_string is %s\n", out_string);
-//printf("OK2\n");
-//printf("LEN is %d\n", (int) strlen(in_string));
     in_string = realloc(in_string, strlen(in_string) + 2);
-//printf("in_string is %s\n", in_string);
-    strncat(in_string, "0",1);
-//printf("LEN is %d\n", (int) strlen(in_string));
-//printf("OK3\n");
+    strncat(in_string, "0", 1);
     free(row);
   } while ((alpha = alpha / BASE));
+
+#ifdef DEBUG
 printf("LEAVING mul_int_string with alpha of %d and in_string of %s and outstring of %s\n", alpha, in_string, out_string);
+#endif
   
   return 0;
 }
