44ca2c5db9e108d0434b2a04280712e051b39bf8
Bob Wilkinson <bob@fourtheye.org>
2012-10-09 17:34:01 +0100

    working

--- c/mul_digit_string.c
+++ c/mul_digit_string.c
@@ -5,12 +5,12 @@
 
 #define BASE 10
 
-int mul_digit_string(const int digit, const char *in_string, char *out_string)
+int mul_digit_string(const int digit, const char *in_string, char **out_string_ptr)
 {
   /* multiplies the in_string by a single digit */
 
 #ifdef DEBUG
-printf("entering mul_digit_string digit is %d input is %s output is %s\n", digit, in_string, out_string);
+printf("entering mul_digit_string digit is %d input is %s output is %s\n", digit, in_string, *out_string_ptr);
 #endif
 
   assert(digit >= 0);
@@ -27,20 +27,24 @@ printf("entering mul_digit_string digit is %d input is %s output is %s\n", digit
 
     int product = in * digit + carry;
     int new = product % BASE;
-    *(out_string + i - 1) = new + '0';
+    *(*out_string_ptr + i - 1) = new + '0';
     carry = product / BASE;
   }
 
   if (carry)
   {
-    out_string = realloc(out_string, strlen(out_string)+1);
-    for (i=strlen(out_string)+1; i>0; --i)
-      *(out_string+i) = *(out_string+i-1);
-    *out_string = carry + '0';
+    int out_len = strlen(*out_string_ptr);
+    char *tmp = calloc(out_len + 1, sizeof(char));
+    strncpy(tmp, *out_string_ptr, out_len);
+    *out_string_ptr = realloc(*out_string_ptr, out_len + 2);
+    *(*out_string_ptr) = carry + '0';
+    *(*out_string_ptr+1) = 0;
+    strncat(*out_string_ptr, tmp, strlen(tmp));
+    free(tmp);
   }
 
 #ifdef DEBUG
-printf("leaving mul_digit_string output is %s\n", out_string);
+printf("leaving mul_digit_string output is %s\n", *out_string_ptr);
 #endif
 
   return 0;
