8203e93c88e7a5ec0ec7a2ad58d1fc427f3784af
Bob Wilkinson <bob@fourtheye.org>
2012-10-09 08:06:02 +0000

    use constant in_string

--- c/mul_int_string.c
+++ c/mul_int_string.c
@@ -8,7 +8,7 @@
 int mul_digit_string(const int, const char *, char *);
 int add_digit_strings(char *, char *, char *);
 
-int mul_int_string(int alpha, char *in_string, char *out_string)
+int mul_int_string(int alpha, const char *in_string, char *out_string)
 {
 #ifdef DEBUG
 printf("ENTERING mul_int_string with alpha of %d and in_string of %s and outstring of %s\n", alpha, in_string, out_string);
@@ -16,23 +16,25 @@ printf("ENTERING mul_int_string with alpha of %d and in_string of %s and outstri
 
   /* multiplies the input string in_string by the integer alpha */
 
-  int in_len = strlen(in_string);
-  strncpy(out_string, "0", 2);
   int in_length = strlen(in_string); 
+  char *tmp = calloc(in_length + 1, sizeof(char));
+  strncpy(tmp, in_string, in_length);
+  strncpy(out_string, "0", 2);
 
   do {
     int modulo = alpha % BASE;
 //    printf("ALPHA is %d and MODULO is %d\n", alpha, modulo);
     char *row = (char *) calloc(in_length + 1, sizeof(char));
-    int mds_ret = mul_digit_string(modulo, in_string, row);
+    int mds_ret = mul_digit_string(modulo, tmp, row);
     assert (mds_ret == 0);
     add_digit_strings(row, out_string, out_string);
-    in_string = realloc(in_string, strlen(in_string) + 2);
-    strncat(in_string, "0", 1);
+    tmp = realloc(tmp, strlen(tmp) + 2);
+    strncat(tmp, "0", 1);
     free(row);
   } while ((alpha = alpha / BASE));
-  in_string = realloc(in_string, in_len + 1);
-  *(in_string + in_len) = 0;
+//  in_string = realloc(in_string, in_length + 1);
+//  *(in_string + in_length) = 0;
+  free(tmp);
 
 #ifdef DEBUG
 printf("LEAVING mul_int_string with alpha of %d and in_string of %s and outstring of %s\n", alpha, in_string, out_string);
