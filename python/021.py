e58828e2ed830e7846bafa267bd9bb9a180bc6b2
Bob Wilkinson <bob@fourtheye.org>
2012-10-11 16:54:58 +0100

    new files

---
+++ python/021.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from math import sqrt
+
+def main():
+  """ this computes projecteuler.net problem 021 """
+
+  SIZE = 10000
+  spd = []
+  for i in range(SIZE):
+    spd.append(sum_factors(i))
+
+  result = 0
+  for i in range(SIZE):
+    if (spd[i] < SIZE):
+      if (i != spd[i]):
+        if (spd[spd[i]] == i):
+          result += i
+
+  print result
+  return 0
+
+def sum_factors(number):
+  total = 1
+  for i in range (2, int(sqrt(number))):
+    if ((number % i) == 0):
+      total += (i + (number / i))
+
+  return total
+
+if __name__ == "__main__":
+    main()
