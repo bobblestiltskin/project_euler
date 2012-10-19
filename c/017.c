aa48fa43f2881c524ddbd94c5f8fb6e77937d1c4
Bob Wilkinson <bob@fourtheye.org>
2012-10-19 18:05:11 +0100

    drop comments since we write problem out anyway

--- c/017.c
+++ c/017.c
@@ -6,14 +6,6 @@
 
 int main()
 {
-/*
-If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
-
-If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
-
-NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
-*/
-
   char *units[UNITS] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
   char *tens[TENS] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
   "twenty", "twentyone", "twentytwo", "twentythree", "twentyfour", "twentyfive", "twentysix", "twentyseven", "twentyeight", "twentynine",
--- python/017.py
+++ python/017.py
@@ -3,17 +3,6 @@
 def main():
   """ this computes projecteuler.net problem 017 """
 
-# If the numbers 1 to 5 are written out in words: one, two, three, four, five,
-# then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
-
-# If all the numbers from 1 to 1000 (one thousand) inclusive were written out
-# in words, how many letters would be used?
-
-# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and
-# forty-two) contains 23 letters and 115 (one hundred and fifteen) contains
-# 20 letters. The use of "and" when writing out numbers is in compliance with
-# British usage.
-
   onek = "one thousand"
   hcount = 7 # "hundred" 
   handcount = 10 # "hundred and "
