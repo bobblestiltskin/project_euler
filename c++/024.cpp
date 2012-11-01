9177ecb7ba1215d5508ba998577ef8864ceb16f8
Bob Wilkinson <bob@fourtheye.org>
2012-11-01 09:09:07 +0000

    apply same optimisations as in c

--- c++/024.cpp
+++ c++/024.cpp
@@ -9,11 +9,29 @@
 #include <string>
 #include <sstream>
 
-int main() {
-  const uint64_t start = 123456789;
-  const uint64_t end = 9876543210;
+/*
+  9! is 362880 so after all the 10 digit numbers which 
+  start with 0 or 1 we will have passed 725760 of the 
+  combinations. The millionth combination will start
+  with a 2, since we need to count 274240 more combinations
+  after 2000000000.
 
-  uint64_t count = 0;
+  8! is 40320, so there are that many 10 digit combinations starting 20.
+  The combination starting 22 is invalid (since the numerals must be
+  distinct), so we could count 6 sets of combinations of 40320 up to
+  2700000000 and then we would have seen 725760+241920 leaving 32320 to
+  find.
+
+  7! is 5040, so 6 sets are 30240. As before we count 6 sets from 
+  2700000000 (excluding 2 and 7) and get to 2780000000, leaving 2080 to find.
+*/
+
+int main() {
+  uint64_t start = 2000000000; 
+  start += 780000000; 
+  const uint64_t end   = 9876543210;
+
+  uint64_t count = 725760 + 241920 + 30240;
   uint64_t i;
   for (i = start; i <= end; ++i) {
     std::string digits;
--- python/024.py
+++ python/024.py
@@ -3,10 +3,25 @@
 def main():
   """ this computes projecteuler.net problem 024 """
 
-  start = 123456789
-  end = 9876543210
+#  9! is 362880 so after all the 10 digit numbers which 
+#  start with 0 or 1 we will have passed 725760 of the 
+#  combinations. The millionth combination will start
+#  with a 2, since we need to count 274240 more combinations
+#  after 2000000000.
+#
+#  8! is 40320, so there are that many 10 digit combinations starting 20.
+#  The combination starting 22 is invalid (since the numerals must be
+#  distinct), so we could count 6 sets of combinations of 40320 up to
+#  2700000000 and then we would have seen 725760+241920 leaving 32320 to
+#  find.
+#
+#  7! is 5040, so 6 sets are 30240. As before we count 6 sets from 
+#  2700000000 (excluding 2 and 7) and get to 2780000000, leaving 2080 to find.
 
-  count = 0
+  start = 2780000000
+  end   = 9876543210
+
+  count = 725760 + 241920 + 30240
   i = start - 1
   while (i <= end):
     i += 1
