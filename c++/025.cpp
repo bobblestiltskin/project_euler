484e0a98b5b553ba2fb9f7a6266b6f077e1170af
Bob Wilkinson <bob@fourtheye.org>
2012-11-05 17:07:29 +0000

    use numstring class

--- c++/025.cpp
+++ c++/025.cpp
@@ -7,23 +7,24 @@
 #include <string>
 #include <vector>
 
-void fib(const int, std::vector< std::string > *);
-std::string add_digit_strings(const std::string, const std::string);
+#include "./numstring.h"
+
+void fib(const int, std::vector< numstring > *);
 
 int main() {
   const unsigned int maxlen = 1000;
 
   int i = 0;
-  std::vector< std:: string > fib_vector;
+  std::vector< numstring > fib_vector;
   fib(i, &fib_vector);
-  while (fib_vector.at(i).length() < maxlen)
+  while (fib_vector.at(i).get().length() < maxlen)
     fib(++i, &fib_vector);
 
   printf("%d\n", i);
   return(0);
 }
 
-void fib(const int num, std::vector< std::string > *fib_vector) {
+void fib(const int num, std::vector< numstring > *fib_vector) {
 /* assumes that it is called in a loop with num starting at 0 */
   if (num < 2) {
     std::string digits;
@@ -33,7 +34,8 @@ void fib(const int num, std::vector< std::string > *fib_vector) {
 
     fib_vector->push_back(digits);
   } else {
-    fib_vector->push_back(add_digit_strings(fib_vector->at(num - 2),
-                                            fib_vector->at(num - 1)));
+    numstring tmp(fib_vector->at(num - 2));
+    tmp.add_digit_strings(fib_vector->at(num - 1));
+    fib_vector->push_back(tmp);
   }
 }
