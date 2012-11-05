d1802110ea51701fcfbfd937167d495412524b29
Bob Wilkinson <bob@fourtheye.org>
2012-11-05 17:10:01 +0000

    use numstring class

--- c++/test_factorial.cpp
+++ c++/test_factorial.cpp
@@ -1,10 +1,11 @@
 // Copyright 2012 Bob Wilkinson <bob@fourtheye.org>
 
 #include <cstdio>
-#include <string>
 #include <sstream>
 
-std::string factorial(const int);
+#include "./numstring.h"
+
+numstring factorial(const int);
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -15,12 +16,12 @@ int main(int argc, char **argv) {
   std::stringstream ssout(argv[1]);
   ssout >> maxa;
 
-  std::string fac_string = factorial(maxa);
+  numstring fac_string = factorial(maxa);
   printf("main AFTER maxa is %d and FACTORIAL is %s\n",
-         maxa, fac_string.c_str());
+         maxa, fac_string.get().c_str());
   int sum = 0;
-  for (unsigned int i = 0; i < fac_string.length(); ++i)
-    sum += fac_string.at(i) - '0';
+  for (unsigned int i = 0; i < fac_string.get().length(); ++i)
+    sum += fac_string.get().at(i) - '0';
   printf("SUM is %d\n", sum);
 
   return(0);
