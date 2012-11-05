97473b16b83c041a678f8f6233fe19b3983a03b7
Bob Wilkinson <bob@fourtheye.org>
2012-11-05 18:01:13 +0000

    use overloaded operator

--- c++/test_mul_int_string.cpp
+++ c++/test_mul_int_string.cpp
@@ -43,7 +43,7 @@ void call_mul_int_string(const int index,
   int a = scalars.at(index);
   numstring b = numeric_input.at(index);
 
-  b.mul_int_string(a);
+  b *= a;
   if (b.get() != numeric_output.at(index).get())
     printf("ERROR : %d * %s is %s and should be %s\n", a,
            numeric_input.at(index).get().c_str(), b.get().c_str(),
