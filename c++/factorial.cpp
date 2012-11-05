0d11c507284fea97d6640a73b0fe01b453085ccd
Bob Wilkinson <bob@fourtheye.org>
2012-11-05 17:08:35 +0000

    use numstring class

--- c++/factorial.cpp
+++ c++/factorial.cpp
@@ -1,22 +1,21 @@
 // Copyright 2012 Bob Wilkinson <bob@fourtheye.org>
 
 #include <cstdio>
-#include <string>
 
-std::string mul_int_string(const int, const std::string);
+#include "./numstring.h"
 
-std::string factorial(int num) {
+numstring factorial(int num) {
 #ifdef DEBUG
 printf("ENTERING factorial BEFORE is %d\n", num);
 #endif
 
-  std::string fac_string = "1";
+  numstring fac_string("1");
   for (int i = 0; i < num; ++i)
-    fac_string = mul_int_string(i+1, fac_string);
+    fac_string.mul_int_string(i+1);
 
 #ifdef DEBUG
 printf("LEAVING factorial AFTER num is %d and FACTORIAL is %s\n",
-num, fac_string.c_str());
+num, fac_string.get().c_str());
 #endif
 
   return(fac_string);
