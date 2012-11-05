aaf60958c6aa1093285a06a6eced3a9dd4694c75
Bob Wilkinson <bob@fourtheye.org>
2012-11-05 17:06:04 +0000

    use numstring class

--- c++/020.cpp
+++ c++/020.cpp
@@ -7,21 +7,17 @@
 #include <sstream>
 #include <cassert>
 
-const int BASE = 10;
+#include "./numstring.h"
 
-std::string factorial(const int);
-std::string mul_int_string(const int, const std::string);
-std::string mul_digit_string(const int, const std::string);
-std::string add_digit_strings(const std::string, const std::string);
-std::string add_strings_short_to_long(const std::string, const std::string);
+numstring factorial(const int);
 
 int main() {
   const int maxa = 100;
 
-  std::string fac_string = factorial(maxa);
+  numstring fac_string = factorial(maxa);
   int sum = 0;
-  for (unsigned int i = 0; i < fac_string.length(); ++i)
-    sum += fac_string.at(i) - '0';
+  for (unsigned int i = 0; i < fac_string.get().length(); ++i)
+    sum += fac_string.get().at(i) - '0';
   printf("%d\n", sum);
 
   return(0);
