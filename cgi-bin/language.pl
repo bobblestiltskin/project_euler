4ce12491ab1f57674cd7b8ba20627243a684d47a
Bob Wilkinson <bob@fourtheye.org>
2012-11-15 17:33:57 +0000

    use PEcgi.pm module for common code

--- cgi-bin/language.pl
+++ cgi-bin/language.pl
@@ -8,6 +8,7 @@ use CGI;
 use DirHandle;
 use LWP::UserAgent;
 use XML::LibXML;
+use PEcgi qw(display_file get_web_page decode_web_page get_problem_as_string);
 
 my $extensions = {
   asm    => 's',
@@ -37,11 +38,11 @@ if ((defined $language) and (grep {/^$language$/} keys %$extensions)) {
         if ($stem =~ /^\d+$/) {
           print get_problem_as_string($stem);
         }
-        display_file($dir, $file);
+        display_file($query, $dir, $file);
       }
       if (($extensions->{$language} =~ /^c/) && ($file =~ /\.h$/)) {
 # print header files for c and c++
-        display_file($dir, $file);
+        display_file($query, $dir, $file);
       }
     }
   }
@@ -50,57 +51,3 @@ if ((defined $language) and (grep {/^$language$/} keys %$extensions)) {
   print "Need a language as an argument!!!", "\n";
 }
 print $query->end_html;
-
-sub display_file {
-  my $dir = shift;
-  my $file = shift;
-
-  print '<pre class="example">' . "\n";
-  print "<p><b>$file</b></p>\n";
-  chomp($file = join('/', $dir, $file));
-  open(my $fh, "<", $file) or print "Cannot open ",$file,": $!";
-  while (<$fh>) {
-    s/\&/&amp;/g;
-    s/</&lt;/g;
-    s/>/&gt;/g;
-    print;
-  }
-  close $fh;
-  print $query->end_pre;
-}
-
-sub get_web_page {
-  my $url = shift;
-
-  my $ua = LWP::UserAgent->new;
-  $ua->agent('bob@fourtheye.org::problem_text_grabber');
-
-  my $req = HTTP::Request->new('GET', $url);
-  my $res = $ua->request($req);
-
-  if ($res->is_error()) {
-    carp "Error getting $url: ", $res->status_line, "\n";
-    return undef;
-  } else {
-    $res->content;
-  }
-}
-
-sub decode_web_page {
-  my $html = shift;
-
-  my $xp = XML::LibXML->load_html(string => $html);
-  return ($xp->findnodes('//*[@role="problem"]'))[0];
-}
-
-sub get_problem_as_string {
-  my $number = shift;
-
-  print '<a href="http://projecteuler.net/problem=', $number, '">Description of problem</a>',"\n";
-
-  my $wanted = decode_web_page(get_web_page("http://projecteuler.net/problem=$number"));
-  my $string = $wanted->toString;
-  $string =~ s/\&#13;//g;
-  $string =~ s|href="|href="http://projecteuler.net/|g;
-  return $string;
-}
