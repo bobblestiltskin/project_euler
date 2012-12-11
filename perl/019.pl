222f67443c5ff027b60e0084148ae1d1e06f5402
Bob Wilkinson <bob@fourtheye.org>
2012-12-11 15:50:03 +0000

    drop problem text

--- perl/019.pl
+++ perl/019.pl
@@ -1,19 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
 
-#You are given the following information, but you may prefer to do some research for yourself.
-#
-#    1 Jan 1900 was a Monday.
-#    Thirty days has September,
-#    April, June and November.
-#    All the rest have thirty-one,
-#    Saving February alone,
-#    Which has twenty-eight, rain or shine.
-#    And on leap years, twenty-nine.
-#    A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
-#
-#How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
-
 my $dpm = [ qw( 31 28 31 30 31 30 31 31 30 31 30 31 ) ];
 my $ldpm = [ qw( 31 29 31 30 31 30 31 31 30 31 30 31 ) ];
 
