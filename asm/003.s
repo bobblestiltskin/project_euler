0b11003a5201d4136ebcdb6a060d4c49c0978c34
Bob Wilkinson <bob@fourtheye.org>
2012-12-28 18:17:59 +0000

    new files

---
+++ asm/003.s
@@ -0,0 +1,74 @@
+.syntax unified
+
+@ 600851475143 is 8BE589EAC7 in hex
+@ root 600851475143 is 775146.099
+
+.align 4
+.equ numhi,139		@ 0x8b
+.equ numlo,3851020999	@ 0xe589eac7
+.equ root,775146
+
+@ factorisation algorithm 
+@ initialise m and n to 775146 then increment n until m*n >= x
+@ if m*n == x then we have found 2 factors - both m and n so we
+@ can check if either is prime 
+@ else we decrement m and n and then start to increment n until
+@ our termination condition is met.
+
+num_hi		.req r4
+num_lo		.req r5
+m		.req r6
+n		.req r7
+tmp		.req r8
+
+.section .rodata
+	.align	2
+resstring:
+	.asciz "%d\n"
+.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	stmfd	sp!, {r4-r9, lr}
+
+	ldr	m, =root
+	ldr	n, =root
+	ldr	tmp, =numlo
+loop:
+	umull	num_lo, num_hi, m, n
+	cmp	num_hi, #numhi
+	bgt	decrement_m
+	blt	increment_n
+	cmp	num_lo, tmp
+	blt	increment_n
+	bgt	decrement_m
+
+	mov	r0, n
+	bl	isprime
+	teq	r0, 1
+	moveq	r1, n
+	beq	printme
+
+	mov	r0, m
+	bl	isprime
+	teq	r0, 1
+	moveq	r1, m
+	beq	printme
+
+increment_n:
+	add	n,n,1
+	b	loop
+
+decrement_m:
+	subs	m,m,1
+	subgt	n,n,1
+	b	loop
+	
+printme:
+	ldr	r0, =resstring	@ store address of start of string to r0
+	bl	printf
+	ldmfd	sp!, {r4-r9, pc}
+
+	mov	r7, 1		@ set r7 to 1 - the syscall for exit
+	swi	0		@ then invoke the syscall from linux
---
+++ asm/isprime.s
@@ -0,0 +1,68 @@
+.syntax unified
+
+# this subroutine returns 1 if the passed number is prime; 0 if not
+#
+# inputs
+#   r0 - integer to test
+#
+# outputs
+#   r0 - prime boolean
+#
+# local
+#
+	number		.req r4
+	primeflag	.req r5
+	tmp		.req r6
+	divisor		.req r7
+	running		.req r8
+#  
+.global isprime
+.type isprime, %function
+.text
+.align	2
+#
+isprime:
+	stmfd	sp!, {r4-r9, lr}
+	mov	number, r0
+	ands	tmp, number, 1
+	bne	odd
+	
+	mov	r0, 0
+	cmp	number, 2 	@ 2 is the only prime even number
+	bne	last
+	mov	r0, 1
+	b	last
+
+odd:
+	cmp 	number, 8
+	bgt	big
+	mov	r0, 1
+	cmp	number, 1	@ 1 is the only odd number < 8 not prime
+	bne	last
+	mov	r0, 0
+	b	last
+
+big:
+	mov	divisor, 3
+	mov	running, number
+bigloop:
+	subs	running, running, divisor
+	bgt	bigloop
+	beq	factor
+	add	divisor, divisor, 2
+	mov	running, number
+	mul	tmp, divisor, divisor
+	subs	tmp, tmp, number
+	ble	bigloop
+
+divide_end:
+	mov	r0, 1
+	b	last
+
+factor:
+	mov	r0, 0
+
+last:
+	ldmfd	sp!, {r4-r9, pc}
+	mov	r7, 1		@ set r7 to 1 - the syscall for exit
+	swi	0		@ then invoke the syscall from linux
