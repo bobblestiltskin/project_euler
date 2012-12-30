5802527c02e277989fac32dedd23ac623d320af0
Bob Wilkinson <bob@fourtheye.org>
2012-12-30 21:57:50 +0000

    drop os call

--- asm/isprime.s
+++ asm/isprime.s
@@ -64,5 +64,5 @@ factor:
 
 last:
 	ldmfd	sp!, {r4-r9, pc}
-	mov	r7, 1		@ set r7 to 1 - the syscall for exit
-	swi	0		@ then invoke the syscall from linux
+#	mov	r7, 1		@ set r7 to 1 - the syscall for exit
+#	swi	0		@ then invoke the syscall from linux
