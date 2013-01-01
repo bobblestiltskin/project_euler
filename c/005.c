ab44229ec5ad998a5df7fba0b1f0a422d1be8aea
Bob Wilkinson <bob@fourtheye.org>
2013-01-01 15:32:09 +0000

    re-implement post asm

--- c/005.c
+++ c/005.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
 
+#define MAX 20
+
+unsigned int isprime(unsigned int);
+
 int main()
 {
-  int primes[8] = {2, 3, 5, 7, 11, 13, 17, 19};
-/*
-maximum power of 2 in 1..20 is 16 or 2^4
-maximum power of 3 in 1..20 is 9 or 3^2
-maximum power of other primes < 20 is n^1
-*/
-  int result = 2 * 2 * 2 * 3;
-/* we initialise the result to the powers of 2 and 3 above 1 */
-  int i;
-  for (i=0; i<8; i++)
-    result *= primes[i];
-
-  printf("%d\n",result);
-  
+  unsigned int i = 2;
+  unsigned int try_product = 1;
+  unsigned int total = 1;
+  while (i <= MAX) {
+    if (isprime(i)) {
+      if (try_product) {
+        if ((i * i) > MAX) {
+          try_product = 0;
+        } else {
+          unsigned int tmp = i;
+          unsigned int last = tmp;
+          while (tmp <= MAX) {
+            last = tmp;
+            tmp *= i;
+          }
+          total *= last;
+        }
+      }
+      if (!(try_product))
+        total *= i;
+    }
+    i++;
+  }
+  printf("%d\n",total);
   return 0;
+}
+
+unsigned int isprime(unsigned int num) {
+  if (num % 2) {
+    if (num < 8) {
+      return !(num == 1);
+    } else {
+      unsigned int divisor = 3;
+      while ((divisor * divisor) <= num) {
+        if (!(num % divisor))
+          return 0;
+        divisor += 2;
+      }
+      return 1;
+    }
+  } else {
+    return num == 2;
+  }
 }
