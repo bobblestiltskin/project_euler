7cb29bd8c275cb45bd3f0ac4bfe48e3fede6f484
Bob Wilkinson <bob@fourtheye.org>
2013-01-01 15:55:05 +0000

    reimplement after asm

--- c++/005.cpp
+++ c++/005.cpp
@@ -3,25 +3,54 @@
 // This is a solution for http://projecteuler.net/problem=5
 
 #include <cstdio>
-#include <vector>
 
-int main() {
-  const int iprimes[8] = {2, 3, 5, 7, 11, 13, 17, 19};
-  const std::vector<int> primes(iprimes,
-                                iprimes + sizeof(iprimes) / sizeof(iprimes[0]));
-/*
-maximum power of 2 in 1..20 is 16 or 2^4
-maximum power of 3 in 1..20 is 9 or 3^2
-maximum power of other primes < 20 is n^1
-*/
-  int result = 2 * 2 * 2 * 3;
-/* we initialise the result to the powers of 2 and 3 above 1 */
-  for (std::vector<int>::const_iterator it = primes.begin();
-       it < primes.end();
-       it++)
-    result *= *it;
+const unsigned int MAX=20;
 
-  printf("%d\n", result);
+bool isprime(unsigned int);
 
+int main()
+{
+  unsigned int i = 2;
+  unsigned int try_product = 1;
+  unsigned int total = 1;
+  while (i <= MAX) {
+    if (isprime(i)) {
+      if (try_product) {
+        if ((i * i) > MAX) {
+          try_product = 0;
+        } else {
+          unsigned int tmp = i;
+          unsigned int last = tmp;
+          while (tmp <= MAX) {
+            last = tmp;
+            tmp *= i;
+          }
+          total *= last;
+        }
+      }
+      if (!(try_product))
+        total *= i;
+    }
+    i++;
+  }
+  printf("%d\n",total);
   return 0;
+}
+
+bool isprime(unsigned int num) {
+  if (num % 2) {
+    if (num < 8) {
+      return !(num == 1);
+    } else {
+      unsigned int divisor = 3;
+      while ((divisor * divisor) <= num) {
+        if (!(num % divisor))
+          return 0;
+        divisor += 2;
+      }
+      return 1;
+    }
+  } else {
+    return num == 2;
+  }
 }
