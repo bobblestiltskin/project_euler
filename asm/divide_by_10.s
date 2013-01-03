d07e6f24497a76bed1a2a227da2d194b95a4004a
Bob Wilkinson <bob@fourtheye.org>
2013-01-03 11:14:13 +0000

    rename divide_by_10 to divide_by_10_remainder since it returns both dividend and remainder. add another function called divide_by_10 which returns just the dividend.

--- asm/divide_by_10.s
+++ asm/divide_by_10.s
@@ -1,7 +1,7 @@
 .syntax unified
 
-# this subroutine divides the passed number by 10
-# derived from looking at the output of gcc -S
+# this subroutine divides the passed number by 10 and
+# returns the dividend and remainder
 # 
 # The const -0x33333333 is 0xccccccd (2s complement)
 # 0xcccccccc is 12/15th (0.8) of 0xffffffff and we use this as
@@ -20,11 +20,12 @@
 #   r1 - the remainder 
 
 .equ const,-0x33333333
+#.equ const,0xcccccccd
 .text
 .align	2
-.global	divide_by_10
-.type	divide_by_10, %function
-divide_by_10:
+.global	divide_by_10_remainder
+.type	divide_by_10_remainder, %function
+divide_by_10_remainder:
 	stmfd	sp!, {lr}
 	ldr	r1, =const
 	umull	r2, r3, r1, r0
@@ -35,5 +36,33 @@ divide_by_10:
 	mov	r3, r3, asl #1	@ r3 = 2 * r3 
 	rsb	r3, r3, r0	@ r3 = r0 - r3 = r0 - 10*int(r0/10)
 	mov	r1, r3		@ the remainder
+	mov	r0, r2		@ the dividend
+	ldmfd	sp!, {pc}
+
+
+# this subroutine divides the passed number by 10
+# returns the dividend
+# 
+# The const -0x33333333 is 0xccccccd (2s complement)
+# 0xcccccccc is 12/15th (0.8) of 0xffffffff and we use this as
+# a multiplier, then shift right by 3 bits (divide by 8) to 
+# effect a multiplication by 0.1
+#
+# We multiply this number by 10 (multiply by 4, add 1 then multiply by 2)
+#
+# inputs
+#   r0 - integer to divide
+#
+# outputs
+#   r0 - the dividend 
+
+.align	2
+.global	divide_by_10
+.type	divide_by_10, %function
+divide_by_10:
+	stmfd	sp!, {lr}
+	ldr	r1, =const
+	umull	r2, r3, r1, r0
+	mov	r2, r3, lsr #3	@ r2 = r3 / 8 == r0 / 10
 	mov	r0, r2		@ the dividend
 	ldmfd	sp!, {pc}
