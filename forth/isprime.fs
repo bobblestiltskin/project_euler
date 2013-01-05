043fca4419af5cd228d126fb0aa9a83500010bd5
Bob Wilkinson <bob@iceland.fourtheye.org>
2013-01-05 19:11:42 +0000

    new file

---
+++ forth/isprime.fs
@@ -0,0 +1,28 @@
+: isprime ( n -- isprime )
+dup 2 mod ( check for even)
+if 
+  dup 8 < ( check for < 8 - all odd are prime apart from 1)
+  if 
+    dup 1 = 1+ ( check for == 1)
+  else
+    3 ( start with divisor of 3) 
+    begin
+      2dup mod ( check for n mod k)
+      if
+        2dup dup * > ( check for k*k > n)
+        if 
+          2 + 0  ( bump k and restart loop)
+        else 
+          1 1 ( set isprime and terminate loop)
+        then
+      else
+        0 1 ( clear isprime and terminate loop)
+      then
+    until
+    swap drop 
+  then
+else 
+  dup 2 = 0= 1+ ( check for == 2 )
+then
+swap drop ( drop all but isprime)
+;
