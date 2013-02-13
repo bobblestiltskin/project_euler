4665665ae0866fecae5616264326339184197b22
Bob Wilkinson <bob@fourtheye.org>
2013-02-13 19:29:12 +0000

    test < 10 too and use macros

--- asm/test_divide_by_10.s
+++ asm/test_divide_by_10.s
@@ -2,10 +2,23 @@
 .equ num3,783
 .equ num7,7847
 .equ num8,78
+.equ num9,7
 
 .syntax unified
 
-.macro number num
+.macro dividend num
+	ldr	r1, =\num
+	ldr	r0, =numstring
+	bl	printf
+
+	ldr	r0, =\num
+	bl	divide_by_10
+	mov	r1, r0
+	ldr	r0, =dividendstring
+	bl	printf
+.endm
+
+.macro remainder num
 	ldr	r1, =\num
 	ldr	r0, =numstring
 	bl	printf
@@ -13,7 +26,7 @@
 	ldr	r0, =\num
 	bl	divide_by_10_remainder
 	mov	r2, r0
-	ldr	r0, =laststring
+	ldr	r0, =remainderstring
 	bl	printf
 .endm
 
@@ -21,7 +34,9 @@
 	.align	2
 numstring:
 	.asciz "num is %d\n"
-laststring:
+dividendstring:
+	.asciz "dividend is %d\n"
+remainderstring:
 	.asciz "remainder is %d and dividend is %d\n"
 
 .text
@@ -29,10 +44,17 @@ laststring:
 	.global	main
 	.type	main, %function
 main:
-	number  num0
-	number  num7
-	number  num3
-	number  num8
+	remainder  num0
+	remainder  num7
+	remainder  num3
+	remainder  num8
+	remainder  num9
+
+	dividend  num0
+	dividend  num7
+	dividend  num3
+	dividend  num8
+	dividend  num9
 
 	mov	r0, 0
 	mov	r7, 1		@ set r7 to 1 - the syscall for exit
