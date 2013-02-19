93404b8b42ddab0e87199acd9de172d54a63d2c7
Bob Wilkinson <bob@fourtheye.org>
2013-02-19 18:08:15 +0000

    test with 0 and 1 too

--- asm/test_mul_digit_string.s
+++ asm/test_mul_digit_string.s
@@ -21,6 +21,8 @@
 .equ	oLENGTH,iLENGTH+1
 .equ	opLENGTH,oLENGTH+1
 
+.equ	scalar0,0
+.equ	scalar1,1
 .equ	scalar2,2
 .equ	scalar3,3
 .equ	scalar8,8
@@ -55,25 +57,11 @@ main:
 	bl	printf
 
 	multiplystring scalar3
+	multiplystring scalar0
+	multiplystring scalar1
 	multiplystring scalar8
 	multiplystring scalar2
 
 	mov	r0, 0
 	mov	r7, 1		@ set r7 to 1 - the syscall for exit
 	swi	0		@ then invoke the syscall from linux
-
-# printbytes takes input pointer in r0, input length in r1 and writes printable vector to r2 (with trailing null)
-	.global printbytes
-	.type	printbytes, %function
-printbytes:
-	stmfd	sp!, {lr}
-loopstart:
-	ldrb	r3, [r0], 1
-	add	r3, r3, 48
-	strb	r3, [r2], 1
-	subs	r1, r1, 1
-	bne	loopstart
-
-	mov	r3, 0
-	strb	r3, [r2], 1
-	ldmfd	sp!, {pc}
