91bdd6662d9aaf3e3b039f27510ab3cd9bd38053
Bob Wilkinson <bob@fourtheye.org>
2013-03-07 12:43:56 +0000

    replace naive implementation by the eabi implementation __aeabi_uidivmod defined in http://infocenter.arm.com/help/topic/com.arm.doc.ihi0043d/IHI0043D_rtabi.pdf

--- asm/divide.s
+++ asm/divide.s
@@ -1,12 +1,9 @@
 .syntax unified
-
-# divide takes value in r0, divisor in r1 and returns modulus in r2 and dividend in r3
+# divide takes value in r0, divisor in r1 and returns dividend in r0 and modulus in r1 
 	.global	divide
 	.type	divide, %function
 divide:
-	mov	r3, 0
-divideloop:
-	subs	r0, r0, r1
-	addge	r3, r3, 1
-	bge	divideloop
-	add	r2, r0, r1
+        stmfd   sp!, {lr}
+# see http://infocenter.arm.com/help/topic/com.arm.doc.ihi0043d/IHI0043D_rtabi.pdf
+	bl	__aeabi_uidivmod
+	ldmfd   sp!, {pc}
