046edae9077452205cb1e28579918d823ce27d4d
Bob Wilkinson <bob@fourtheye.org>
2013-03-08 14:56:05 +0000

    use long_divide sub

--- asm/003.s
+++ asm/003.s
@@ -6,18 +6,11 @@
 .align 4
 .equ numhi,139		@ 0x8b
 .equ numlo,3851020999	@ 0xe589eac7
-.equ root,775146
-
-@ factorisation algorithm 
-@ initialise m and n to 775146 then increment n until m*n >= x
-@ if m*n == x then we have found 2 factors - both m and n so we
-@ can check if either is prime 
-@ else we decrement m and n and then start to increment n until
-@ our termination condition is met.
+.equ root,775147
 
 num_hi		.req r4
 num_lo		.req r5
-m		.req r6
+maxdiv		.req r6
 n		.req r7
 tmp		.req r8
 
@@ -30,46 +23,45 @@ resstring:
 	.global	main
 	.type	main, %function
 main:
-	stmfd	sp!, {r4-r9, lr}
+	stmfd	sp!, {r4-r8, lr}
 
-	ldr	m, =root
 	ldr	n, =root
-	ldr	tmp, =numlo
+	ldr	num_lo, =numlo
+	ldr	num_hi, =numhi
 loop:
-	umull	num_lo, num_hi, m, n
-	cmp	num_hi, #numhi
-	bgt	decrement_m
-	blt	increment_n
-	cmp	num_lo, tmp
-	blt	increment_n
-	bgt	decrement_m
-
+        ldr     r0, =numlo
+        ldr     r1, =numhi
+        mov     r2, n
+        mov     r3, 0
+        bl      long_divide
+	teq	r2, 0
+	bne	nextn
+	teq	r3, 0
+	bne	nextn
+	mov	tmp, r0
+	bl	isprime
+	teq	r0, 1
+	bne	testn
+	cmp	maxdiv, tmp
+	movlt	maxdiv, tmp
+testn:
 	mov	r0, n
 	bl	isprime
 	teq	r0, 1
-	moveq	r1, n
+	bne	nextn
+	cmp	maxdiv, n
+	movlt	maxdiv, n
+nextn:
+	sub	n, n, 2
+	teq	n, 1
 	beq	printme
-
-	mov	r0, m
-	bl	isprime
-	teq	r0, 1
-	moveq	r1, m
-	beq	printme
-
-increment_n:
-	add	n,n,1
 	b	loop
-
-decrement_m:
-	subs	m,m,1
-	subgt	n,n,1
-	b	loop
-	
 printme:
+	mov	r1, maxdiv
 	ldr	r0, =resstring	@ store address of start of string to r0
 	bl	printf
 
 	mov	r0, 0
-	ldmfd	sp!, {r4-r9, pc}
+	ldmfd	sp!, {r4-r8, pc}
 	mov	r7, 1		@ set r7 to 1 - the syscall for exit
 	swi	0		@ then invoke the syscall from linux
