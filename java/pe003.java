a113f23e0fd7ea2b11053bba23795f0d1916fb74
Bob Wilkinson <bob@fourtheye.org>
2013-07-04 16:07:18 +0100

    optimisation after asm

--- java/pe003.java
+++ java/pe003.java
@@ -21,18 +21,15 @@ public class pe003 {
   }
 
   public static void main( String[] args ) {
-    final BigInteger big_number = new BigInteger("600851475143");
+    BigInteger big_number = new BigInteger("600851475143");
     int max_prime = 0;
     int counter = 2;
-    BigInteger counter_squared = BigInteger.valueOf(counter).pow(2);
-    while (counter_squared.compareTo(big_number) < 0) {
+    while (big_number.intValue() != 1) {
       if (big_number.mod(BigInteger.valueOf(counter)).intValue() == 0) {
-        BigInteger divi = big_number.divide(BigInteger.valueOf(counter));
         max_prime = set_max_prime(BigInteger.valueOf(counter), max_prime);
-        max_prime = set_max_prime(divi, max_prime);
+        big_number = big_number.divide(BigInteger.valueOf(counter));
       }
       counter++;
-      counter_squared = BigInteger.valueOf(counter).pow(2);
     }
     System.out.println(max_prime);
   }
