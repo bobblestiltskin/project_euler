59cf23e238943dfccaba412b3c76fbccc33e0896
Bob Wilkinson <bob@fourtheye.org>
2013-07-30 16:42:07 +0100

    more functional approach

--- racket/001.rkt
+++ racket/001.rkt
@@ -1,8 +1,12 @@
 #! /usr/bin/env racket
 #lang racket
-(define mysum 0)
-(define (add-to-sum i)
-  (set! mysum (+ mysum i)))
-(for ([i (in-range 1 1000)])
-  (if (or (= (modulo i 3) 0) (= (modulo i 5) 0)) (add-to-sum i) null))
-(printf (string-append (number->string mysum) "\n"))
+(define (add-item i sum)
+  (cond
+    [(< i 1) sum]
+    [else (if (or (= (modulo i 3) 0) (= (modulo i 5) 0))
+             (add-item (- i 1) (+ sum i)) 
+             (add-item (- i 1) sum))
+    ]
+  )
+)
+(add-item 999 0)
