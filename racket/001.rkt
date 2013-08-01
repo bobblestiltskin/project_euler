cba2769689370e598129e7d316fec88ca5c78eca
Bob Wilkinson <bob@fourtheye.org>
2013-08-01 14:18:50 +0100

    simplify cond

--- racket/001.rkt
+++ racket/001.rkt
@@ -3,10 +3,8 @@
 (define (add-item i sum)
   (cond
     [(< i 1) sum]
-    [else (if (or (= (modulo i 3) 0) (= (modulo i 5) 0))
-             (add-item (- i 1) (+ sum i)) 
-             (add-item (- i 1) sum))
-    ]
+    [(or (= (modulo i 3) 0) (= (modulo i 5) 0)) (add-item (- i 1) (+ sum i))]
+    [else (add-item (- i 1) sum)]
   )
 )
 (add-item 999 0)
