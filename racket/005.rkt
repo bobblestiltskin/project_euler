9f6245d8df0c7d540644399bb29fc6d8a8d73119
Bob Wilkinson <bob@fourtheye.org>
2013-11-13 19:37:47 +0000

    new files

---
+++ racket/005.rkt
@@ -0,0 +1,38 @@
+#lang racket
+(define number 20)
+
+; prime? adapted from http://danf.wordpress.com/2011/03/05/testing-for-primes-scheme/
+
+(define (divides? a b)
+  (zero? (remainder a b)))
+
+(define (prime? n)
+  (cond 
+    [(or (= n 1) (= n 0)) #f]
+    [(= n 2) #t]
+    [(even? n) #f]
+    [else 
+      (let prime-test ([d 3])
+        (cond 
+          [(> (* d d) n) #t]
+          [(divides? n d) #f]
+          [else (prime-test (+ d 2))]
+        )
+      )
+    ]
+  )
+)
+
+(define (max-base-m-lt-n m last n)
+  (if (> (* m last) n)
+     last
+     (max-base-m-lt-n m (* m last) n)
+  )
+)
+
+(for/fold ([lcm 1])([i (in-range 2 (add1 number))])
+  (if (prime? i)
+     (* lcm (max-base-m-lt-n i 1 number))
+     lcm
+  )
+)
---
+++ racket/006.rkt
@@ -0,0 +1,26 @@
+#lang racket
+(define mini 1)
+(define maxi 100)
+
+(define (ap-sum mini maxi)
+; arithmetic progression sum = (max-min+1)*(max+min)/2
+  (/ (* (+ maxi mini) (add1 (- maxi mini))) 2)
+)
+
+(define (sq-sum mini maxi)
+  (let ((tmp (ap-sum mini maxi)))
+    (* tmp tmp)
+  )
+)
+ 
+(define (sum-sq mini maxi)
+  (for/fold ([sumsq 0])([i (in-range mini (add1 maxi))])
+    (+ sumsq (* i i))
+  )
+)
+
+(define (sumdiff sumsq sqsum)
+  (abs (- sumsq sqsum))
+)
+
+(sumdiff (sq-sum mini maxi) (sum-sq mini maxi))
