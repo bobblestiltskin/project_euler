50680326aca558f6b32538f6c59a33f976a2f06e
Bob Wilkinson <bob@fourtheye.org>
2013-12-13 22:07:44 +0000

    new file

---
+++ racket/019.rkt
@@ -0,0 +1,36 @@
+#! /usr/bin/env racket
+#lang racket
+
+(define january 31)
+(define february 28)
+(define march 31)
+(define april 30)
+(define may 31)
+(define june 30)
+(define july 31)
+(define august 31)
+(define september 30)
+(define october 31)
+(define november 30)
+(define december 31)
+(define leap-february 29)
+
+(define year (list january february march april may june july 
+                   august september october november december))
+(define leap-year (list january leap-february march april may june july 
+                        august september october november december))
+(define cycle (flatten (list year year year leap-year)))
+(define num-cycles 25)
+
+;cycle
+
+(define (process-month m daycount daynum)
+  ;(printf "m is ~s, c is ~s, n is ~s, r is ~s\n" m daycount daynum (remainder (+ daynum m) 7))
+  (if (zero? (remainder (+ daynum m) 7))
+    (values (add1 daycount) (+ m daynum))
+    (values daycount (+ m daynum))
+  )
+)
+
+(define-values (c n) (for*/fold ([daycount 0][daynum 2]) ([i (in-range 25)][m cycle]) (process-month m daycount daynum)))
+c
