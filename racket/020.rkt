b1f04819aeaeb9e4d600b5dd28da5af1d67916b0
Bob Wilkinson <bob@fourtheye.org>
2013-12-14 22:22:34 +0000

    new file

---
+++ racket/020.rkt
@@ -0,0 +1,17 @@
+#! /usr/bin/env racket
+#lang racket
+
+(define num 100)
+
+(define (factorial n)
+  (cond
+    [(= 1 n) 1]
+    [else (* n (factorial (sub1 n)))]))
+
+(define (sum-digits n sofar)
+  (cond
+    [(< n 10) (+ n sofar)]
+    [else (let-values ([(q r) (quotient/remainder n 10)])
+          (sum-digits q (+ r sofar)))]))
+
+(sum-digits (factorial num) 0)
