b860bc075793a4014c490c83038c1970a1a36818
Bob Wilkinson <bob@fourtheye.org>
2014-01-27 21:31:54 +0000

    better since unsure of precedence of logical operators

--- erlang/pe001.erl
+++ erlang/pe001.erl
@@ -5,7 +5,7 @@
 pe001(N) ->
   pe001(N, 0).
   
-pe001(N, Result) when (N > 0) and (N rem 3 == 0) or (N rem 5 == 0) ->
+pe001(N, Result) when (N > 0) and ((N rem 3 == 0) or (N rem 5 == 0)) ->
     pe001(N-1, N+Result);
     
 pe001(N, Result) when N > 0 ->
