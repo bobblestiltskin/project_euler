8b779eb1164a28f8952d61dd39491ad750233fed
Bob Wilkinson <bob@fourtheye.org>
2014-01-27 22:57:41 +0000

    allow make to run problem

--- erlang/pe001.erl
+++ erlang/pe001.erl
@@ -1,9 +1,10 @@
 -module(pe001).
--export([pe001/1]). 
+-export([start/0]).
 
+max() -> 999.
 
-pe001(N) ->
-  pe001(N, 0).
+start() ->
+  pe001(max(), 0).
   
 pe001(N, Result) when (N > 0) and ((N rem 3 == 0) or (N rem 5 == 0)) ->
     pe001(N-1, N+Result);
@@ -12,4 +13,4 @@ pe001(N, Result) when N > 0 ->
     pe001(N-1, Result);
     
 pe001(N, Result) ->
-    io:format("~n"), Result.
+    io:format("~w~n", [Result]).
