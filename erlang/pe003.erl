4307dc17d2aa240bdac830501469f3496ebab342
Bob Wilkinson <bob@fourtheye.org>
2014-01-28 22:51:11 +0000

    new file

---
+++ erlang/pe003.erl
@@ -0,0 +1,25 @@
+-module(pe003).
+-export([start/0]).
+
+maxv() -> 600851475143.
+
+start() ->
+  pe003(maxv()).
+  
+pe003(Number) when (Number == 2) ->
+  io:format("~w~n", [2]);
+    
+pe003(Number) when (Number rem 2 == 0) ->
+  pe003(Number div 2);
+    
+pe003(Number) ->
+  loop003(3, Number).
+
+loop003(Start, Number) when (Number == 1) ->
+  io:format("~w~n", [Start - 2]);
+
+loop003(Start, Number) when (Number rem Start == 0) ->
+  loop003(Start + 2, Number div Start);
+
+loop003(Start, Number) ->
+  loop003(Start + 2, Number).
