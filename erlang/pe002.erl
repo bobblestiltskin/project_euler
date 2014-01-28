db5a9a45979fe18861149ec889c4d439cf97b32d
Bob Wilkinson <bob@fourtheye.org>
2014-01-28 21:28:38 +0000

    new file

---
+++ erlang/pe002.erl
@@ -0,0 +1,16 @@
+-module(pe002).
+-export([start/0]).
+
+maxv() -> 4000000.
+
+start() ->
+  pe002(0, 1, 0, maxv()).
+  
+pe002(Prev, Current, Sum, Maxv) when (Current >= Maxv) ->
+    io:format("~w~n", [Sum]);
+    
+pe002(Prev, Current, Sum, Maxv) when (Current rem 2 == 0) ->
+    pe002(Current, Prev+Current, Sum+Current, Maxv);
+    
+pe002(Prev, Current, Sum, Maxv) ->
+    pe002(Current, Prev+Current, Sum, Maxv).
