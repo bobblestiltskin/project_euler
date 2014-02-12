8f92c22516c5e4b8604b110e27babc27349fdce6
Bob Wilkinson <bob@fourtheye.org>
2014-02-12 18:46:08 +0000

    new file

---
+++ erlang/pe014.erl
@@ -0,0 +1,28 @@
+-module(pe014).
+-export([start/0]).
+
+max() -> 1000000.
+
+start() ->
+  pe014(0, 0, 1, max()).
+  
+pe014(Maxv, Maxi, I, M) when I < M ->
+  Next = pe014(0, I),
+  if 
+    Next > Maxv ->
+      pe014(Next, I, I + 1, M);
+    true ->
+      pe014(Maxv, Maxi, I + 1, M)
+  end;
+      
+pe014(_Maxv, Maxi, _I, _M) ->
+  io:format("~w~n", [Maxi]).
+    
+pe014(C, N) when N == 1 ->
+  C;
+
+pe014(C, N) when N rem 2 == 0 ->
+  pe014(C + 1, N div 2);
+    
+pe014(C, N) ->
+  pe014(C + 1, 3 * N + 1).
