e526362082033bee143f37206f44ad733b0c996a
Bob Wilkinson <bob@fourtheye.org>
2014-12-12 14:06:20 +0000

    need to force 64 bit arithmetic on 32 bit platforms
     enter the commit message for your changes. Lines starting

--- ocaml/pe003.ml
+++ ocaml/pe003.ml
@@ -1,13 +1,16 @@
 open Core.Std
 
-let divisible x y  = x mod y = 0 ;;
+let divisible x y  = 
+  Int64.(rem x y = zero) ;;
 
 let rec pe003 n i =
-  match n with
-  | 1 -> printf "%d\n" (i-2)
-  | _ -> if divisible n i then
-           pe003 (n/i) (i+2)
-         else
-           pe003 n (i+2) ;;
+  let open Int64 in
+    if n = one then
+      printf "%Lu\n" (i - of_int 2)
+    else
+      if (divisible n i) then
+        pe003 (n / i) (i + of_int 2)
+      else
+        pe003 n (i + of_int 2) ;;
 
-pe003 600851475143 3
+pe003 600851475143L 3L ;;
