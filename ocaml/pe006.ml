0a25738066b84935eac907d98b258b1965e8f4dd
Bob Wilkinson <bob@fourtheye.org>
2014-12-17 16:51:12 +0000

    new code - direct transposition of erlang

---
+++ ocaml/pe006.ml
@@ -0,0 +1,21 @@
+open Core.Std
+
+let maxv = 100 ;;
+
+let ap_sum x y =
+  ((x + y) * ((y - x) + 1)) / 2 ;;
+
+let sq_sum x y =
+  let z = ap_sum x y in
+    z * z ;;
+
+let rec sum_sq i max sum =
+  if i > max then
+    sum
+  else
+    sum_sq (i + 1) max (sum + (i * i)) ;;
+   
+let pe006 i max =
+  printf "%d\n" (abs((sq_sum i max) - (sum_sq i max 0))) ;;
+
+pe006 1 maxv ;;
