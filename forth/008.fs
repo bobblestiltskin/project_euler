0e56f6e0a860dca1fdeafef4efbe0e6304032e67
Bob Wilkinson <bob@fourtheye.org>
2015-01-09 19:14:09 +0000

    extend to use 13 values - need to extend to 64 bit storage

--- forth/008.fs
+++ forth/008.fs
@@ -65,18 +65,24 @@ create data 7 c, 3 c, 1 c, 6 c, 7 c, 1 c, 7 c, 6 c, 5 c, 3 c, 1 c, 3 c, 3 c,
 2 c, 0 c, 7 c, 5 c, 2 c, 9 c, 6 c, 3 c, 4 c, 5 c, 0 c, 
 
 : pe008
-0 988 0 do 
-  13 0 do
+0. 988 0 do
+  7 0 do
     i j data + + c@
   loop
-  * * * * * * * * * * * * 2dup
-  <
+  * * * * * * ( multiply 7 numbers - will be ok in 32 bits)
+  13 7 do
+    i j data + + c@
+  loop
+  * * * * * ( and then 6 numbers )
+  m* ( and multiply these 2 32 bit numbers and store to 64 bits )
+  2over 2over
+  d<
   if
-    swap 
+    2swap 
   then
-  drop
+  2drop
 loop
-. cr
+d. cr
 ;
 
 pe008
