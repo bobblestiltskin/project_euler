cf273a5277d1d4c16e2ad637100b636c9e6f8e1b
Bob Wilkinson <bob@fourtheye.org>
2015-01-27 23:03:54 +0000

    use even function

--- ocaml/pe014.ml
+++ ocaml/pe014.ml
@@ -3,18 +3,20 @@ open Int64
 
 let million = 1000000L ;;
 
+let even x = rem x 2L = zero ;;
+
 let rec collatz c n =
   if n = one then
     c
   else 
-    if (rem n 2L = zero) then
+    if even n then
       collatz (c + one) (n / 2L)
     else
       collatz (c + one) ((3L * n) + one) ;;
      
 let rec pe014 maxv maxi i m =
   if i < m then
-    let next = (collatz 0L i) in
+    let next = (collatz zero i) in
       if next > maxv then
         pe014 next i (i + one) m
       else
