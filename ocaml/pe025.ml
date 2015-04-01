f6d831d3bac1ca5db3c847612f0253cd75f90aa5
Bob Wilkinson <bob@fourtheye.org>
2015-04-01 21:44:43 +0100

    new file

---
+++ ocaml/pe025.ml
@@ -0,0 +1,13 @@
+open Core.Std
+
+let maxnum = 1000 ;;
+
+let rec pe025 i j n =
+  let k = Big_int.add_big_int i j in
+  let numlen = String.length (Big_int.string_of_big_int k) in
+  if numlen < maxnum then
+    pe025 j k (n + 1)
+  else
+    n ;;
+
+printf "%d\n" (pe025 Big_int.zero_big_int Big_int.unit_big_int 2) ;;
