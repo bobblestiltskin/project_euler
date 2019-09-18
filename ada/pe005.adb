622bb1aa83c6eab0af4e89bb577652bc45f2f96c
Bob Wilkinson <bob@fourtheye.org>
2019-09-18 17:06:34 +0100

    drop inline definition of isprime since we use a module, called Prime

--- ada/pe005.adb
+++ ada/pe005.adb
@@ -1,37 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Prime; use Prime;
-procedure pe005 is
 
---function isprime(num : Integer) return boolean is
---
---  divisor : Integer;
---
---  begin
---    if (num mod 2 = 1) then
---      if (num < 8) then
---        if (num = 1) then
---          return false;
---        else
---          return true;
---        end if;
---      else
---        divisor := 3;
---        while ((divisor * divisor) <= num) loop
---          if (num mod divisor = 0) then
---            return false;
---          end if;
---          divisor := divisor + 2;
---        end loop;
---        return true;
---      end if;
---    else
---      if (num = 2) then
---        return true;
---      else
---        return false;
---      end if;
---    end if;
---end isprime;
+procedure pe005 is
 
 i           : Integer := 2;
 max         : constant Integer := 20;
