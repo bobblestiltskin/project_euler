01248577aef29d1776243bc0d5314fb7bada39a7
Bob Wilkinson <bob@fourtheye.org>
2020-09-03 19:50:51 +0100

    implementation of first 5 PE problems in JavaScript

---
+++ javascript/003.js
@@ -0,0 +1,16 @@
+let number = 600851475143;
+let max_prime = 0;
+let i = 3;
+
+while (number != 1)
+{
+  if (number % i)
+    i+=2;
+  else
+  {
+    max_prime = i;
+    number = number/i;
+  }
+}
+
+console.log("%d", max_prime);
