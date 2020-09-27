c6a9b2b6b028f7460aa8fff6f8cdd8f1cb819b41
Bob Wilkinson <bob@fourtheye.org>
2020-09-27 20:25:28 +0100

    forst implementations of pe008 and pe009 in rust

---
+++ rust/pe009/src/main.rs
@@ -0,0 +1,20 @@
+fn pe009() -> u64 {
+    let stop:u64 = 1000;
+
+    for a in 0 .. stop {
+        for b in a+1 .. stop {
+            for c in b+1 .. stop {
+                if a+b+c == stop {
+                    if a*a + b*b == c*c {
+                        return a*b*c
+                    }
+                }
+            }
+        }
+    }
+    0
+}
+
+fn main() {
+    println!("{}", pe009())
+}
