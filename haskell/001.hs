e7dab51aba9c27f4da598544eae3308a3e496e69
Bob Wilkinson <bob@fourtheye.org>
2020-10-16 18:36:14 +0100

    renamed

--- haskell/001.hs
+++
@@ -1,1 +0,0 @@
-main = print ( sum [ x | x <- [0..999] , x `mod` 3 == 0 || x `mod` 5 == 0] )
--- haskell/002.hs
+++
@@ -1,6 +0,0 @@
-fibonacci :: ( Integral a ) => a -> a
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
-
-main = print(sum (takeWhile (<4000000) (filter even (map fibonacci [1..]))))
