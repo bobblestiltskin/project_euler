0ce500d14eb8b54c8a532ad378c702aea84ff77d
Bob Wilkinson <bob@fourtheye.org>
2021-11-10 10:29:28 +0000

    drop debug line

--- perl/pe009.pl
+++ perl/pe009.pl
@@ -6,7 +6,6 @@ for (my $a = 1; $a < $stop; $a++) {
   for (my $b = $a + 1; $b < $stop; $b++) {
     for (my $c = $b + 1; $c < $stop; $c++) {
       if ((($a + $b +$c) == $stop) and (($a * $a) + ($b * $b) == ($c * $c))) {
-        print "a is $a, b is $b, c is $c\n";
         print $a*$b*$c,"\n";
         exit(0);
       }
