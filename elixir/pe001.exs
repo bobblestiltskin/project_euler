08455ebc2dc07ecad405475c764c2123db841d03
Bob Wilkinson <bob@fourtheye.org>
2021-11-22 20:28:46 +0000

    use guard for testing positivity

--- elixir/pe001.exs
+++ elixir/pe001.exs
@@ -1,15 +1,14 @@
 defmodule Pe001 do
-  def sum_multiples(n, sum) do
-    if n > 0 do
-#      IO.puts(n)
-      if rem(n, 3) == 0 || rem(n, 5) == 0 do
-        sum_multiples(n - 1, sum + n)
-      else 
-        sum_multiples(n - 1, sum)
-      end    
-    else 
-      IO.puts(sum)
-    end    
+  def sum_multiples(n, sum) when n > 0 do
+    if rem(n, 3) == 0 || rem(n, 5) == 0 do
+      sum_multiples(n - 1, sum + n)
+    else
+      sum_multiples(n - 1, sum)
+    end
+  end
+
+  def sum_multiples(_, sum) do
+    IO.puts(sum)
   end
 end
 
