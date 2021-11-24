92f6ed481b4cdd68c80051e7bd3a54bc0734cfcf
Bob Wilkinson <bob@fourtheye.org>
2021-11-24 18:50:51 +0000

    more elegant?

--- elixir/pe002.exs
+++ elixir/pe002.exs
@@ -3,12 +3,12 @@ defmodule ProjectEuler do
     sum_even_fibonacci(1, 1, n, 0)
   end
    
+  defp sum_even_fibonacci(previous, current, n, sum) when current < n and rem(current, 2) == 0 do
+    sum_even_fibonacci(current, previous + current, n, sum + current)
+  end
+
   defp sum_even_fibonacci(previous, current, n, sum) when current < n do
-    if rem(current, 2) == 1 do
-      sum_even_fibonacci(current, previous + current, n, sum)
-    else
-      sum_even_fibonacci(current, previous + current, n, sum + current)
-    end
+    sum_even_fibonacci(current, previous + current, n, sum)
   end
 
   defp sum_even_fibonacci(_, _, _, sum) do
