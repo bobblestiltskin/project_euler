80d6c3ca6ae84f509873d441efe7a6d0691b37b2
Robert Wilkinson <bob@fourtheye.org>
2024-03-27 18:29:22 +0000

    another

---
+++ haskell/pe020.hs
@@ -0,0 +1,14 @@
+import Data.Char
+
+mysum :: String -> Int -> Int
+mysum [] n = n
+mysum (x:xs) n = mysum xs (digitToInt(x) + n)
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+pe020:: Integer -> Int
+pe020 n = mysum (show (factorial n)) 0
+
+main = print(pe020 100)
