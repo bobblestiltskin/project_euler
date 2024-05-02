2b5620b6e119014f91df7d767cbecb9d6f856141
Robert Wilkinson <bob@fourtheye.org>
2024-05-02 09:22:56 +0100

    pe010 in ada

---
+++ ada/pe010.adb
@@ -0,0 +1,30 @@
+pragma Ada_2022;
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings; use Ada.Strings;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Numerics.Big_Numbers.Big_Integers; use Ada.Numerics.Big_Numbers.Big_Integers;
+
+with Prime; use Prime;
+
+procedure pe010 is
+
+last : constant Integer := 2000000;
+primes : prime_array(1 .. last) := (1 => 2, 2 => 3, others => 0);
+
+test   : Integer;
+vsize  : Integer := 2;
+psum   : Big_Integer := 5;
+
+begin
+  test := primes(vsize) + 2;
+  while test < last loop
+    if isprime_array(test, primes) then
+      vsize := vsize + 1;
+      primes(vsize) := test;
+      psum := psum + To_Big_Integer(test);
+    end if;
+    test := test + 2;
+  end loop;
+  Put (Ada.Strings.Fixed.Trim(Big_Integer'Image(psum), Side => Both));
+end pe010;
