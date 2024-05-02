0c9eeddb175128026aee053d89725ca23df286bf
Robert Wilkinson <bob@fourtheye.org>
2024-05-02 08:32:59 +0100

    add early loop exit and return status

--- ada/pe009.adb
+++ ada/pe009.adb
@@ -1,21 +1,26 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Command_Line;
 
 procedure pe009 is
 
 stop   : constant Integer := 1000;
 
 begin
+Outer_Loop:
   for a in 1 .. stop loop
     for b in (a+1) .. stop loop
       for c in (b+1) .. stop loop
         if (a+b+c) = stop then
           if ((a*a)+(b*b)) = (c*c) then
             Put (Ada.Strings.Fixed.Trim(Integer'Image(a*b*c), Side => Both));
+            Ada.Command_Line.Set_Exit_Status(0);
+            exit Outer_Loop;
           end if;
         end if;
       end loop;
     end loop;
-  end loop;
+    Ada.Command_Line.Set_Exit_Status(1);
+  end loop Outer_Loop;
 end pe009;
