610f577033b9897d40756149bdc55014152dfd9c
Robert Wilkinson <bob@fourtheye.org>
2024-05-02 13:05:14 +0100

    drop commented code

--- rust/project_euler_020/src/main.rs
+++ rust/project_euler_020/src/main.rs
@@ -1,8 +1,6 @@
 extern crate num_bigint;
-//extern crate num_traits;
 
 use num_bigint::{BigUint,ToBigUint};
-//use num_bigint::{Zero, One};
 
 fn pe020() -> BigUint {
     let zero:BigUint = "0".parse::<BigUint>().unwrap();
@@ -11,13 +9,11 @@ fn pe020() -> BigUint {
     const LAST:usize = 100;
     let ten:BigUint = "10".parse::<BigUint>().unwrap();
     let mut factorial:BigUint = one;
-    //let mut factorial:BigUint = One::one();
 
     for i in 1 .. LAST {
         factorial = &factorial * i.to_biguint().unwrap();
     }
 
-    //let mut fsum:BigUint = Zero::zero();
     let mut fsum:BigUint = zero;
     while factorial > ten {
         fsum = fsum + &factorial % &ten;
