3d2b61d966c14c193c40f0f3369d2090ecfed847
Robert Wilkinson <bob@fourtheye.org>
2024-05-08 17:46:06 +0100

    pe021 in ada

---
+++ ada/pe021.adb
@@ -0,0 +1,44 @@
+--  this computes projecteuler.net problem 021
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings; use Ada.Strings;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Numerics.Elementary_Functions;  use Ada.Numerics.Elementary_Functions;
+with Ada.Containers.Vectors;
+
+procedure pe021 is
+
+package One_D_Vec is new Ada.Containers.Vectors (Index_Type   => Positive, Element_Type => Integer);
+
+spd  : One_D_Vec.Vector;
+
+SIZE: constant Integer := 10000;
+result: Integer := 0;
+
+function sum_factors(num : Integer) return Integer is
+  total : Integer := 1;
+  upper : Integer := Integer(Float'Floor(Sqrt(Float(num))));
+  begin
+    for i in 2 .. upper loop
+      if num mod i = 0 then
+        total := total + (i + (num / i));
+      end if;
+    end loop;
+    return total;
+end sum_factors;
+
+begin
+  for i in 1 .. SIZE loop
+    spd.Append(sum_factors(i));
+  end loop;
+  for i in 1 .. SIZE loop
+     if spd(i) < SIZE then
+       if i /= spd(i) then
+         if spd(spd(i)) = i then
+           result := result + i;
+         end if;
+       end if;
+     end if;
+  end loop;
+  Put (Ada.Strings.Fixed.Trim(Integer'Image(result), Side => Both));
+end pe021;
