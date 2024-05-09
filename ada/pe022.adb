d651ba9d6417e6a33019308454590f5fa755674e
Robert Wilkinson <bob@fourtheye.org>
2024-05-09 09:00:27 +0100

    working pe022 code for ada, but more work needed

--- ada/pe022.adb
+++ ada/pe022.adb
@@ -9,18 +9,15 @@ with Ada.Containers.Generic_Sort;
 
 procedure pe022 is
 
-MAXNAMES: constant Integer := 50;
-
-type Unbounded_String_Array is array (Positive range <>) of Unbounded_String;
-
-namesa:  Unbounded_String_Array(1 .. MAXNAMES) := (others => To_Unbounded_String(""));
-tmp_string : Unbounded_String := To_Unbounded_String("");
-
-Input_File: File_Type;
-
-package String_Vec is new Ada.Containers.Vectors (Index_Type   => Positive, Element_Type => Unbounded_String);
+package String_Vec is new Ada.Containers.Vectors (Index_Type => Positive, Element_Type => Unbounded_String);
 names  : String_Vec.Vector := String_Vec.To_Vector(1);
-names_length  : Integer := 0;
+
+MAXNAMES: constant Integer := 50000;
+type Unbounded_String_Array is array (Positive range <>) of Unbounded_String;
+namesa:  Unbounded_String_Array(1 .. MAXNAMES) := (others => To_Unbounded_String(""));
+
+-- using the namesa variable inside these function bodies looks ... different
+-- but they work and http://www.ada-auth.org/standards/12rat/html/Rat12-8-7.html
 
 function US_Before(L, R: Natural) return Boolean is 
 begin
@@ -28,6 +25,7 @@ begin
 end US_Before;
 
 procedure US_Swap(L, R: in Natural) is
+tmp_string : Unbounded_String := To_Unbounded_String("");
 begin
    tmp_string := namesa(L);
    namesa(L) := namesa(R);
@@ -36,19 +34,31 @@ end US_Swap;
 
 procedure US_G_Sort is new Ada.Containers.Generic_Sort (Positive, Before => US_Before, Swap => US_Swap);
 
+function letter_count(word: Unbounded_String) return Integer is
+  count : Integer := 0;
+  iletter : Integer := 0;
+  letter  : Character;
+
+begin
+  for i in 1 .. Length(word) loop
+    letter := Element(word, i);
+    iletter := CHARACTER'POS(letter) - 64;
+    count := count + iletter;
+  end loop;
+
+  return count;
+end letter_count;
+
 function To_Unbounded_String_Array(V: String_Vec.Vector; A: IN OUT Unbounded_String_Array) return Unbounded_String_Array is
   vlen: Natural := Natural(V.Length);
 begin
-      Put_Line (Integer'Image(vlen));
     for I in 1 .. vlen loop
         A(I) := V.Element(I);
-      Put_Line (To_String(A(I)));
     end loop;
     return A;
 end To_Unbounded_String_Array;
 
 function text_file_to_string_vector(in_file : File_Type) return String_Vec.Vector is
-
   One_Char : CHARACTER;
   building_name: Boolean := false;
   name_string: Unbounded_String := To_Unbounded_String("");
@@ -74,6 +84,11 @@ begin
   return names;
 end text_file_to_string_vector;
 
+Input_File: File_Type;
+names_length  : Integer := 0;
+count: Integer := 0;
+index : Integer := 1;
+
 begin
   Ada.Text_IO.Open (File => Input_File, Mode => In_File, Name => "../names.txt");
   names := text_file_to_string_vector(Input_File);
@@ -81,28 +96,15 @@ begin
 
   namesa := To_Unbounded_String_Array(names, namesa);
   names_length := Integer(names.Length);
-  Put_Line (Integer'Image(names_length));
---  namesa.US_G_Sort(First => 1, Last => names_length);
   US_G_Sort(First => 1, Last => names_length);
 --  for n of names loop
   for n of namesa loop
     if To_String(n)'Length /= 0 then
-    Put_Line (To_String(n));
+--    Put_Line (To_String(n));
+      count := count + (letter_count(n) * index);
+--    Put_Line (Integer'Image(count));
     end if;
+    index := index + 1;
   end loop;
+  Put (Ada.Strings.Fixed.Trim(Integer'Image(count), Side => Both));
 end pe022;
---  names = []
---  start = 1
---  name = name_string[start:len(name_string) - 1]
---  names.append(name)
---  names.sort()
---
---  total = 0
---  for i in range(len(names)):
---    letters = 0
---    for j in range(len(names[i])):
---      letters += ord(names[i][j]) - 64
---    total += letters * (i+1)
---
---  print(total)
---  return 0
