0e6bb3885e2ea5efa593866481d7ef2731c0f18e
Robert Wilkinson <bob@fourtheye.org>
2024-05-13 11:22:35 +0000

    pe006 in arm64 asm

--- asm64/pe006.s
+++ asm64/pe006.s
@@ -1,9 +1,9 @@
 .equ limit,100
 
-number	.req r4
-sumsq	.req r5
-sqsum	.req r6
-tmp	.req r7
+number	.req x4
+sumsq	.req x5
+sqsum	.req x6
+tmp	.req x7
 
 .section .rodata
 	.align	2
@@ -14,11 +14,9 @@ string:
 	.global	main
 	.type	main, %function
 main:
-#	stmfd	sp!, {r4-r7, lr}
-        stp fp, lr, [sp, #-0x40]!
+        stp fp, lr, [sp, #-0x30]!
         stp x4, x5, [sp, #0x10]
         stp x6, x7, [sp, #0x20]
-        stp x8, x9, [sp, #0x30]
         mov fp, sp
 
 	mov	sqsum, 0
@@ -40,16 +38,13 @@ end_loop:
 	mul	sumsq, sumsq, sumsq
 last:
 	sub	tmp, sumsq, sqsum 
-	mov	r1, tmp
-	ldr	r0, =string	/* store address of start of string to r0 */
+	mov	x1, tmp
+	ldr	x0, =string	/* store address of start of string to r0 */
 	bl	printf
 
-	mov	r0, 0
-#	ldmfd	sp!, {r4-r7, pc}
-        ldp x8, x9, [sp, #0x30]
         ldp x6, x7, [sp, #0x20]
         ldp x4, x5, [sp, #0x10]
-        ldp fp, lr, [sp], #0x40
+        ldp fp, lr, [sp], #0x30
 
 	mov	x0, #0		/* exit code to 0 */
 	mov     w8, #93		/* set w8 to 93 - the syscall for exit */
