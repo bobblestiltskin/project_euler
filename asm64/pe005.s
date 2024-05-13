c262effe6e4cc5d8f157a7e92b515fbcb6c2fe3e
Robert Wilkinson <bob@fourtheye.org>
2024-05-13 11:19:55 +0000

    pe005 in arm64 asm

--- asm64/pe005.s
+++ asm64/pe005.s
@@ -13,11 +13,11 @@ set try_product to 0.
 if try_products is 0 and the number is prime, then multiply
 the total by number. */
 
-try_product	.req r4
-number		.req r5
-last		.req r6
-total		.req r7
-tmp		.req r8
+try_product	.req x4
+number		.req x5
+last		.req x6
+total		.req x7
+tmp		.req x8
 
 .section .rodata
 	.align	2
@@ -28,7 +28,6 @@ resstring:
 	.global	main
 	.type	main, %function
 main:
-#	stmfd	sp!, {r4-r8, lr}
         stp fp, lr, [sp, #-0x40]!
         stp x4, x5, [sp, #0x10]
         stp x6, x7, [sp, #0x20]
@@ -39,9 +38,9 @@ main:
 	mov	try_product, 1
 	mov	number, 2
 loop:
-	mov	r0, number
+	mov	x0, number
 	bl	isprime20
-	cmp	r0, 1
+	cmp	x0, 1
 	bne	nexti
 	
 	cmp	try_product, 1
@@ -73,12 +72,10 @@ nexti:
 	b	loop
 	
 printme:
-	mov	r1, total
-	ldr	r0, =resstring	/* store address of start of string to r0 */ 
+	mov	x1, total
+	ldr	x0, =resstring	/* store address of start of string to r0 */ 
 	bl	printf
 
-	mov	r0, 0
-#	ldmfd	sp!, {r4-r8, pc}
         ldp x8, x9, [sp, #0x30]
         ldp x6, x7, [sp, #0x20]
         ldp x4, x5, [sp, #0x10]
@@ -102,39 +99,28 @@ printme:
 .align	2
 
 isprime20:
-#	stmfd	sp!, {lr}
-        stp fp, lr, [sp, #-0x40]!
-        stp x4, x5, [sp, #0x10]
-        stp x6, x7, [sp, #0x20]
-        stp x8, x9, [sp, #0x30]
+        stp fp, lr, [sp]
         mov fp, sp
 
-	mov	r1, r0
-	ands	r2, r1, 1
+	mov	x1, x0
+	ands	x2, x1, 1
 	bne	odd
-	mov	r0, 0
-	cmp	r1, 2 	/* 2 is the only prime even r1 */
+	mov	x0, 0
+	cmp	x1, 2 	/* 2 is the only prime even r1 */
 	bne	last
-	mov	r0, 1
+	mov	x0, 1
 	b	last
 odd:
-	mov	r0, 1
-	cmp	r1, 9
+	mov	x0, 1
+	cmp	x1, 9
 	bne	test15
-	mov	r0, 0
+	mov	x0, 0
 	b	last
 test15:
-	cmp	r1, 15
+	cmp	x1, 15
 	bne	last
-	mov	r0, 0
+	mov	x0, 0
 last:
-#	ldmfd	sp!, {pc}
-        ldp x8, x9, [sp, #0x30]
-        ldp x6, x7, [sp, #0x20]
-        ldp x4, x5, [sp, #0x10]
-        ldp fp, lr, [sp], #0x40
+        ldp fp, lr, [sp]
 
-	mov	x0, #0		/* exit code to 0 */
-	mov     w8, #93		/* set w8 to 93 - the syscall for exit */
-        svc	#0		/* then invoke the syscall from linux */
-
+        ret
