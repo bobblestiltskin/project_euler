bb768c0de59bf733cec6293ab96254f70beba9f2
Robert Wilkinson <bob@fourtheye.org>
2024-05-23 08:14:27 +0000

    no need to save registers

--- asm64/pe024.s
+++ asm64/pe024.s
@@ -3,37 +3,11 @@
 # - see http://en.wikipedia.org/wiki/Factorial_number_system
 # - and http://en.wikipedia.org/wiki/Permutation
 
-.macro save_regs_on_stack
-        stp x20, x21, [sp, #-0x90]!
-        stp x18, x19, [sp, #0x10]
-        stp x16, x17, [sp, #0x20]
-        stp x14, x15, [sp, #0x30]
-        stp x12, x13, [sp, #0x40]
-        stp x10, x11, [sp, #0x50]
-        stp x8, x9,   [sp, #0x60]
-        stp x6, x7,   [sp, #0x70]
-        stp x4, x5,   [sp, #0x80]
-.endm
-
-.macro restore_regs_from_stack
-        ldp x4, x5,   [sp, #0x80]
-        ldp x6, x7,   [sp, #0x70]
-        ldp x8, x9,   [sp, #0x60]
-        ldp x10, x11, [sp, #0x50]
-        ldp x12, x13, [sp, #0x40]
-        ldp x14, x15, [sp, #0x30]
-        ldp x16, x17, [sp, #0x20]
-        ldp x18, x19, [sp, #0x10]
-        ldp x20, x21, [sp], #0x90
-.endm
-
 .macro check_digit a
 	ldr	x0, =vector
 	mov	x1, VSIZE
 	mov	x2, \a
-	save_regs_on_stack
 	bl	contains
-	restore_regs_from_stack
 	cmp	x0, 1
 	bne	mloop
 .endm
@@ -43,14 +17,8 @@
 	add	x0, x0, 1
 	mov	x1, VSIZE
 	ldr	x2, =\b
-	save_regs_on_stack
 	bl	copybytes
-	restore_regs_from_stack
 .endm
-
-# this macro lifted from test_add_digit_strings.s 
-# the usage of add_digit_strings is tricky because
-# the fifth parameter must be passed on the stack
 
 .macro add_strings a al b bl c
         ldr     x0, =\a
@@ -59,9 +27,7 @@
         ldr     x3, =\bl
         ldr     x4, =\c
 
-	save_regs_on_stack
         bl      add_digit_strings
-	restore_regs_from_stack
 .endm
 
 .equ	datum_size, 2
