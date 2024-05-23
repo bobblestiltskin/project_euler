fb20de8f633559967d0c696ee0173a451797a8d6
Robert Wilkinson <bob@fourtheye.org>
2024-05-23 15:49:46 +0000

    drop comments

--- asm64/pe004.s
+++ asm64/pe004.s
@@ -36,19 +36,8 @@ jloop:
 	mul	product, i, j
 
 	mov	x0, product
-#
-#        stp fp, lr, [sp, #-0x40]!
-#        stp x4, x5, [sp, #0x10]
-#        stp x6, x7, [sp, #0x20]
-#        stp x8, x9, [sp, #0x30]
-#        mov fp, sp
 
 	bl	is_palindromic
-
-#        ldp x8, x9, [sp, #0x30]
-#        ldp x6, x7, [sp, #0x20]
-#        ldp x4, x5, [sp, #0x10]
-#        ldp fp, lr, [sp], #0x40
 
 	cmp	x0, #1
 	bne	next
