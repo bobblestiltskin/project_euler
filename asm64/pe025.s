0164fd8e8360df60190a974b627402b7db470039
Robert Wilkinson <bob@fourtheye.org>
2024-05-23 09:02:21 +0000

    pe025 in arm64 asm

--- asm64/pe025.s
+++ asm64/pe025.s
@@ -1,5 +1,29 @@
 # this computes projecteuler.net problem 025
 
+.macro save_regs_on_stack
+        stp x20, x21, [sp, #-0x90]!
+        stp x18, x19, [sp, #0x10]
+        stp x16, x17, [sp, #0x20]
+        stp x14, x15, [sp, #0x30]
+        stp x12, x13, [sp, #0x40]
+        stp x10, x11, [sp, #0x50]
+        stp x8, x9,   [sp, #0x60]
+        stp x6, x7,   [sp, #0x70]
+        stp x4, x5,   [sp, #0x80]
+.endm
+
+.macro restore_regs_from_stack
+        ldp x4, x5,   [sp, #0x80]
+        ldp x6, x7,   [sp, #0x70]
+        ldp x8, x9,   [sp, #0x60]
+        ldp x10, x11, [sp, #0x50]
+        ldp x12, x13, [sp, #0x40]
+        ldp x14, x15, [sp, #0x30]
+        ldp x16, x17, [sp, #0x20]
+        ldp x18, x19, [sp, #0x10]
+        ldp x20, x21, [sp], #0x90
+.endm
+
 .macro copy_vector a b
 	ldr	x0, =\a
 	add	x0, x0, 1
@@ -8,38 +32,16 @@
 	bl	copybytes
 .endm
 
-# this macro lifted from test_add_digit_strings.s 
-# the usage of add_digit_strings is tricky because
-# the fifth parameter must be passed on the stack
-
 .macro add_strings a al b bl c
-#        stmfd   sp!, {r4}       /* stash r4 on the stack - we destroy it in add_digit_strings */
-#        stp fp, lr, [sp, #-0x40]!
-#        stp x4, x5, [sp, #0x10]
-#        stp x6, x7, [sp, #0x20]
-#        stp x8, x9, [sp, #0x30]
-#        mov fp, sp
-
-#        stmfd   sp!, {r0}       /* this is the fifth parameter for the subroutine */
-#        stp fp, lr, [sp, #-0x40]!
-#        stp x4, x5, [sp, #0x10]
-#        stp x6, x7, [sp, #0x20]
-#        stp x8, x9, [sp, #0x30]
-#        mov fp, sp
-
         ldr     x0, =\a
         ldr     x1, =\al
         ldr     x2, =\b
         ldr     x3, =\bl
         ldr     x4, =\c
 
+	save_regs_on_stack
         bl      add_digit_strings
-#        add     sp, sp, 4       /* revert sp to before (1) */
-#        ldmfd   sp!, {r4}      /* and get stashed r4 */
-#        ldp x8, x9, [sp, #0x30]
-#        ldp x6, x7, [sp, #0x20]
-#        ldp x4, x5, [sp, #0x10]
-#        ldp fp, lr, [sp], #0x40
+	restore_regs_from_stack
 .endm
 
 .macro add_and_test ivector ovector
@@ -56,9 +58,9 @@
 
 .align 4
 
-vptr		.req x2
-tmp		.req w3
-icount		.req x4
+vptr		.req x18
+tmp		.req w19
+icount		.req x20
 
 .section .data
 .align  2
@@ -79,11 +81,6 @@ resstring:
 .global main
 .type   main, %function
 main:
-#        stmfd   sp!, {r4, lr}
-#        stp fp, lr, [sp, #-0x40]!
-#        stp x4, x5, [sp, #0x10]
-#        stp x6, x7, [sp, #0x20]
-#        stp x8, x9, [sp, #0x30]
         stp fp, lr, [sp, #-0x10]!
         mov fp, sp
 
@@ -97,11 +94,6 @@ printme:
         ldr     x0, =resstring
         bl      printf
 
-#        ldmfd   sp!, {r4, pc}
-#        ldp x8, x9, [sp, #0x30]
-#        ldp x6, x7, [sp, #0x20]
-#        ldp x4, x5, [sp, #0x10]
-#        ldp fp, lr, [sp], #0x40
         ldp fp, lr, [sp], #0x10
 
 	mov	x0, #0		/* exit code to 0 */
