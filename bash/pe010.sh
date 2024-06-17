6b3687463bb34ff7db6d8eff25a7e02be1934a6a
Robert Wilkinson <bob@fourtheye.org>
2024-06-17 19:11:19 +0100

    very slow pe010 in bash

---
+++ bash/pe010.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# computes project euler problem 010 in bash
+function isprimelist
+{
+# first parameter is test, second parameter is a list of known primes
+# returns 0 if prime , 1 if non-prime
+  TEST=$1
+  PRIMES=$2
+
+  for PRIME in ${PRIMES[@]}
+  do
+    if [ $(($PRIME * $PRIME)) -gt $TEST ]
+    then
+      return 0
+    fi
+    if [ $(($TEST % $PRIME)) = 0 ]
+    then
+      return 1
+    fi
+  done
+}
+MAXPRIME=2000000
+declare -a PRIMES=(2 3)
+TEST=5
+SUM=0
+while [ ${PRIMES[-1]} -le $MAXPRIME ]
+do
+  isprimelist $TEST $PRIMES
+  if [ $? = 0 ]
+  then
+    PRIMES+=($TEST)
+    SUM=$(($SUM + $TEST))
+    echo $TEST
+  fi
+  TEST=$(($TEST + 2))
+done
+echo $SUM
