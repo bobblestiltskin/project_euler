c784f4c02a3feb5051806b29361a8585f1afab42
Robert Wilkinson <bob@fourtheye.org>
2026-05-08 21:06:27 +0100

    no debug

--- bash/pe014.sh
+++ bash/pe014.sh
@@ -11,22 +11,18 @@ function next_term(){
 }
 
 START=500000
-LAST=500100
-#LAST=1000000
+LAST=1000000
 MAX=0
 MAXI=0
 
 for i in $(seq $START $LAST)
 {
-#  echo $i
   COUNTER=0
   j=$i
   while [ $j != 1 ];
   do
-#    echo $j
     j=$(next_term $j)
     COUNTER=$(($COUNTER+1))
-#    echo $j $COUNTER
   done
   COUNTER=$(($COUNTER+1))
   echo $i $COUNTER $MAX
@@ -34,8 +30,6 @@ for i in $(seq $START $LAST)
   then
     MAX=$COUNTER
     MAXI=$i
-#  else
-#    echo $i $COUNTER $MAX
   fi
 }
 echo $MAXI
